d029032fb8dab7827107e7bde6359e27eb797f84
Peter Wu <piterwu@outlook.com>
2020-03-23 16:35:02 +0800

    + dist-plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,9 +5,18 @@ subprojects {
         plugin("com.jfrog.artifactory")
         plugin("org.springframework.boot")
     }
+    dependencies{
+        compile(gradleApi())
+        compile(kotlin("stdlib"))
+
+        testCompile(kotlin("test-junit"))
+    }
     tasks {
         "jar"(Jar::class) {
             enabled = true
+        }
+        "dokkaJavadoc"(org.jetbrains.dokka.gradle.DokkaTask::class) {
+            noStdlibLink = true
         }
         "compileJava" {
             dependsOn("processResources")
---
+++ plugin/dist-plugin/build.gradle.kts
@@ -0,0 +1,6 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
+dependencies {
+    compile("gradle.plugin.com.github.alexeylisyutenko:windows-service-plugin")
+
+}
