f45eaf267915f99221a5e19cd6bcf6d7638e462b
Peter Wu <piterwu@outlook.com>
2020-03-23 16:49:51 +0800

    + publish-plugin

---
+++ plugin/publish-plugin/src/main/kotlin/cn/bestwu/gradle/publish/KotlinPublishPlugin.kt
@@ -0,0 +1,36 @@
+package cn.bestwu.gradle.publish
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
+
+/**
+ *
+ * 注册task
+ *
+ * @author Peter Wu
+ */
+class KotlinPublishPlugin : AbstractPlugin() {
+    /**
+     * {@inheritDoc}
+     */
+    override fun apply(project: Project) {
+        beforeConfigigure(project)
+
+        project.plugins.apply("org.jetbrains.dokka")
+        project.tasks.create("dokkaJavadoc", DokkaTask::class.java) {
+            it.outputFormat = "javadoc"
+            it.outputDirectory = "${project.buildDir}/dokkaJavadoc"
+            it.noStdlibLink = true
+        }
+        project.afterEvaluate { _ ->
+            project.tasks.create("javadocJar", Jar::class.java) {
+                it.classifier = "javadoc"
+                it.from(project.tasks.getByName("dokkaJavadoc").outputs)
+            }
+
+            configPublish(project)
+        }
+    }
+
+}
---
+++ plugin/publish-plugin/src/main/kotlin/cn/bestwu/gradle/publish/PublishPlugin.kt
@@ -0,0 +1,30 @@
+package cn.bestwu.gradle.publish
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.javadoc.Javadoc
+
+/**
+ *
+ * 注册task
+ *
+ * @author Peter Wu
+ */
+class PublishPlugin : AbstractPlugin() {
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun apply(project: Project) {
+        beforeConfigigure(project)
+
+        project.afterEvaluate { _ ->
+            project.tasks.create("javadocJar", Jar::class.java) {
+                it.classifier = "javadoc"
+                it.from(project.tasks.getByName("javadoc").outputs)
+            }
+            configPublish(project)
+        }
+    }
+
+}
