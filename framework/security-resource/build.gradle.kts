38fdba9fad6859625b09e9aab761f3564e03363b
Peter Wu <piterwu@outlook.com>
2020-03-25 17:26:25 +0800

    spring security resource 配置调整

--- framework/security-resource/build.gradle.kts
+++ framework/security-resource/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     compile(project(":framework:security-core"))
     compileOnly("org.springframework.security.oauth:spring-security-oauth2")
 
-
     testCompile(project(":framework:security-server"))
     testCompile("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
     testCompile("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure")
