6050dbe837223f2b59338b5c0896203ba0b56d0b
Peter Wu <piterwu@outlook.com>
2020-04-15 11:48:02 +0800

    Field增加hash及equals方法

--- util/autodoc-core/src/main/kotlin/cn/bestwu/autodoc/core/model/Field.kt
+++ util/autodoc-core/src/main/kotlin/cn/bestwu/autodoc/core/model/Field.kt
@@ -26,6 +26,22 @@ data class Field(
         var partType: String = "",
         var children: LinkedHashSet<Field> = LinkedHashSet()) {
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Field) return false
+
+        if (name != other.name) return false
+        if (type != other.type) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + type.hashCode()
+        return result
+    }
+
     /**
      * 是否必填
      */
