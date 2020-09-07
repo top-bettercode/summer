69d73b40161b9359929b84db860b85199f022e8a
Peter Wu <piterwu@outlook.com>
2020-09-07 14:44:33 +0800

    Excel导入优化

--- util/excel/src/main/java/cn/bestwu/simpleframework/util/excel/ExcelException.java
+++ util/excel/src/main/java/cn/bestwu/simpleframework/util/excel/ExcelException.java
@@ -7,6 +7,10 @@ public class ExcelException extends RuntimeException {
 
   private static final long serialVersionUID = -6806745108191259515L;
 
+  public ExcelException(String message) {
+    super(message);
+  }
+
   public ExcelException(String message, Throwable cause) {
     super(message, cause);
   }
