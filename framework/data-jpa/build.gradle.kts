afa68ef138d2d7795a6e4702c435156dbe1ed218
Peter Wu <piterwu@outlook.com>
2020-09-14 15:38:42 +0800

    Q类生成调整

--- framework/data-jpa/build.gradle.kts
+++ framework/data-jpa/build.gradle.kts
@@ -9,27 +9,8 @@ dependencies {
     api("org.mybatis:mybatis-spring")
     api("com.github.pagehelper:pagehelper")
 
-    annotationProcessor("com.querydsl:querydsl-apt:4.2.1:jpa")
+    testAnnotationProcessor("com.querydsl:querydsl-apt:4.3.1:jpa")
+    testAnnotationProcessor("jakarta.persistence:jakarta.persistence-api:2.2.3")
     testImplementation("com.h2database:h2")
 }
 
-val testquerydslSourcesDir = file("src/testquerydsl/java")
-tasks {
-    create("generateTestJPAQueryDSL", JavaCompile::class.java) {
-        source = fileTree("src/test/java/cn/bestwu/simpleframework/data/test/domain")
-        classpath = configurations.runtimeClasspath.get() + configurations.annotationProcessor.get()
-        options.compilerArgs.addAll(listOf("-proc:only", "-processor", "com.querydsl.apt.jpa.JPAAnnotationProcessor"))
-        options.annotationProcessorPath = classpath
-        options.encoding = "UTF-8"
-        destinationDir = testquerydslSourcesDir
-    }
-    "compileTestJava"(JavaCompile::class) {
-        //        dependsOn("generateTestJPAQueryDSL")
-        source += fileTree(testquerydslSourcesDir)
-    }
-}
-idea {
-    module {
-        testSourceDirs = testSourceDirs + testquerydslSourcesDir
-    }
-}
