c10f716340b3c040dafd5bad539fc575626f405b
Peter Wu <piterwu@outlook.com>
2020-12-02 17:18:55 +0800

    oauth 异常处理

---
+++ framework/security-core/src/main/java/cn/bestwu/simpleframework/security/exception/IllegalUserOauth2Exception.java
@@ -0,0 +1,25 @@
+package cn.bestwu.simpleframework.security.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
+
+@SuppressWarnings("deprecation")
+public class IllegalUserOauth2Exception extends OAuth2Exception {
+
+  private static final long serialVersionUID = -3550061022144911053L;
+
+  public IllegalUserOauth2Exception(String msg, Throwable t) {
+    super(msg, t);
+  }
+
+  @Override
+  public String getOAuth2ErrorCode() {
+    return HttpStatus.BAD_REQUEST.getReasonPhrase();
+  }
+
+  @Override
+  public int getHttpErrorCode() {
+    return 400;
+  }
+
+}
