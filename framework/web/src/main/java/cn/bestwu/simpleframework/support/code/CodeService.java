fd91b857a012aed6cd1878bc1cba4f047ca2bbde
Peter Wu <piterwu@outlook.com>
2021-04-07 15:01:46 +0800

    状态码调整

--- framework/web/src/main/java/cn/bestwu/simpleframework/support/code/CodeService.java
+++ framework/web/src/main/java/cn/bestwu/simpleframework/support/code/CodeService.java
@@ -4,12 +4,15 @@ import cn.bestwu.lang.property.PropertiesSource;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Peter Wu
  */
 public class CodeService implements ICodeService {
 
+  private final Logger log = LoggerFactory.getLogger(CodeService.class);
   private final PropertiesSource propertiesSource;
   private final Map<String, Serializable> codeProperties;
 
@@ -24,19 +27,35 @@ public class CodeService implements ICodeService {
         codeType = split[0];
         String code = split[1];
         if (v != null) {
-          if (code.startsWith("0") && code.length() > 1) {
-            codeProperties.put(codeType + "." + v, code);
-          } else {
+          String type = propertiesSource.getString(codeType + "|TYPE");
+          boolean isInt = type == null ? isInt(code) : "Int".equals(type);
+          if (isInt) {
             try {
               int codeKey = Integer.parseInt(code);
               codeProperties.put(codeType + "." + v, codeKey);
             } catch (NumberFormatException e) {
+              log.warn("状态码解析失败，期望Int类型的状态码");
               codeProperties.put(codeType + "." + v, code);
             }
+          } else {
+            codeProperties.put(codeType + "." + v, code);
           }
         }
       }
     });
+  }
+
+  private boolean isInt(String code) {
+    if (code.startsWith("0") && code.length() > 1) {
+      return false;
+    } else {
+      try {
+        Integer.parseInt(code);
+        return true;
+      } catch (NumberFormatException e) {
+        return false;
+      }
+    }
   }
 
   @Override
