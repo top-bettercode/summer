7c6908d84841390d530bd0bdee5ceee1942e0bbc
Peter Wu <piterwu@outlook.com>
2021-04-20 13:05:51 +0800

    OracleToDDL 主键更新优化

--- util/generator/src/main/kotlin/cn/bestwu/generator/ddl/OracleToDDL.kt
+++ util/generator/src/main/kotlin/cn/bestwu/generator/ddl/OracleToDDL.kt
@@ -8,7 +8,12 @@ object OracleToDDL : ToDDL() {
     override val quoteMark: String = "\""
     override val commentPrefix: String = "--"
 
-    override fun toDDLUpdate(oldTables: List<Table>, tables: List<Table>, out: PrintWriter, deleteTablesWhenUpdate: Boolean) {
+    override fun toDDLUpdate(
+        oldTables: List<Table>,
+        tables: List<Table>,
+        out: PrintWriter,
+        deleteTablesWhenUpdate: Boolean
+    ) {
         if (tables != oldTables) {
             val tableNames = tables.map { it.tableName }
             val oldTableNames = oldTables.map { it.tableName }
@@ -35,26 +40,39 @@ object OracleToDDL : ToDDL() {
                         val columns = table.columns
                         val oldPrimaryKeys = oldTable.primaryKeys
                         val primaryKeys = table.primaryKeys
-                        val oldPrimaryKey = oldPrimaryKeys[0]
-                        val primaryKey = primaryKeys[0]
-                        if (oldPrimaryKeys.size == 1 && primaryKeys.size == 1 && oldPrimaryKey != primaryKey) {
+
+                        if (oldPrimaryKeys.size == 1 && primaryKeys.size == 1) {
+                            val oldPrimaryKey = oldPrimaryKeys[0]
+                            val primaryKey = primaryKeys[0]
                             if (primaryKey.columnName != oldPrimaryKey.columnName)
                                 lines.add("ALTER TABLE $quote$tableName$quote DROP PRIMARY KEY;")
                         }
-
 
                         val oldColumnNames = oldColumns.map { it.columnName }
                         val columnNames = columns.map { it.columnName }
                         val dropColumnNames = oldColumnNames - columnNames
                         if (dropColumnNames.isNotEmpty()) {
-                            lines.add("ALTER TABLE $quote$tableName$quote DROP (${dropColumnNames.joinToString(",") { "$quote$it$quote" }});")
+                            lines.add(
+                                "ALTER TABLE $quote$tableName$quote DROP (${
+                                    dropColumnNames.joinToString(
+                                        ","
+                                    ) { "$quote$it$quote" }
+                                });"
+                            )
                         }
                         dropFk(oldColumns, dropColumnNames, lines, tableName)
                         val newColumnNames = columnNames - oldColumnNames
                         columns.forEach { column ->
                             val columnName = column.columnName
                             if (newColumnNames.contains(columnName)) {
-                                lines.add("ALTER TABLE $quote$tableName$quote ADD ${columnDef(column, quote)};")
+                                lines.add(
+                                    "ALTER TABLE $quote$tableName$quote ADD ${
+                                        columnDef(
+                                            column,
+                                            quote
+                                        )
+                                    };"
+                                )
                                 lines.add("COMMENT ON COLUMN $quote$tableName$quote.$quote$columnName$quote IS '${column.remarks}';")
                                 addFk(column, lines, tableName, columnName)
                             } else {
@@ -69,7 +87,9 @@ object OracleToDDL : ToDDL() {
                                 }
                             }
                         }
-                        if (oldPrimaryKeys.size == 1 && primaryKeys.size == 1 && oldPrimaryKey != primaryKey) {
+                        if (oldPrimaryKeys.size == 1 && primaryKeys.size == 1) {
+                            val oldPrimaryKey = oldPrimaryKeys[0]
+                            val primaryKey = primaryKeys[0]
                             if (primaryKey.columnName != oldPrimaryKey.columnName)
                                 lines.add("ALTER TABLE $quote$tableName$quote ADD PRIMARY KEY(\"$quote${primaryKey.columnName}$quote\" )")
                         }
@@ -93,14 +113,20 @@ object OracleToDDL : ToDDL() {
         if (it.typeDesc.isBlank()) {
             throw Exception("${it.tableSchem ?: ""}:${it.columnName}:字段类型不能为空")
         }
-        val def = "${if (it.typeDesc != old.typeDesc) it.typeDesc else ""}${if (it.defaultDesc != old.defaultDesc) it.defaultDesc else ""}${if (it.extra.isNotBlank() && it.extra != old.extra) " ${it.extra}" else ""}${if (it.autoIncrement && it.autoIncrement != old.autoIncrement) " AUTO_INCREMENT" else ""}${if (old.nullable != it.nullable) if (it.nullable) " NULL" else " NOT NULL" else ""}"
+        val def =
+            "${if (it.typeDesc != old.typeDesc) it.typeDesc else ""}${if (it.defaultDesc != old.defaultDesc) it.defaultDesc else ""}${if (it.extra.isNotBlank() && it.extra != old.extra) " ${it.extra}" else ""}${if (it.autoIncrement && it.autoIncrement != old.autoIncrement) " AUTO_INCREMENT" else ""}${if (old.nullable != it.nullable) if (it.nullable) " NULL" else " NOT NULL" else ""}"
         return if (def.isBlank()) {
             ""
         } else
             "$quote${it.columnName}$quote $def"
     }
 
-    override fun updateIndexes(oldTable: Table, table: Table, lines: MutableList<String>, dropColumnNames: List<String>) {
+    override fun updateIndexes(
+        oldTable: Table,
+        table: Table,
+        lines: MutableList<String>,
+        dropColumnNames: List<String>
+    ) {
         val tableName = table.tableName
         val delIndexes = oldTable.indexes - table.indexes
         if (delIndexes.isNotEmpty()) {
@@ -113,9 +139,21 @@ object OracleToDDL : ToDDL() {
         if (newIndexes.isNotEmpty()) {
             newIndexes.forEach { indexed ->
                 if (indexed.unique) {
-                    lines.add("CREATE UNIQUE INDEX $quote${indexed.name}$quote ON $quote$tableName$quote (${indexed.columnName.joinToString(",") { "$quote$it$quote" }});")
+                    lines.add(
+                        "CREATE UNIQUE INDEX $quote${indexed.name}$quote ON $quote$tableName$quote (${
+                            indexed.columnName.joinToString(
+                                ","
+                            ) { "$quote$it$quote" }
+                        });"
+                    )
                 } else {
-                    lines.add("CREATE INDEX $quote${indexed.name}$quote ON $quote$tableName$quote (${indexed.columnName.joinToString(",") { "$quote$it$quote" }});")
+                    lines.add(
+                        "CREATE INDEX $quote${indexed.name}$quote ON $quote$tableName$quote (${
+                            indexed.columnName.joinToString(
+                                ","
+                            ) { "$quote$it$quote" }
+                        });"
+                    )
                 }
             }
         }
@@ -126,7 +164,13 @@ object OracleToDDL : ToDDL() {
         pw.println("$commentPrefix $tableName")
         if (table.sequenceStartWith != null) {
             pw.println("DROP SEQUENCE $quote${tableName}_S$quote;")
-            pw.println("CREATE SEQUENCE $quote${tableName}_S$quote INCREMENT BY 1 START WITH 1${fill(table.sequenceStartWith!!)};")
+            pw.println(
+                "CREATE SEQUENCE $quote${tableName}_S$quote INCREMENT BY 1 START WITH 1${
+                    fill(
+                        table.sequenceStartWith!!
+                    )
+                };"
+            )
         }
         pw.println()
         pw.println("DROP TABLE $quote$tableName$quote;")
@@ -134,7 +178,14 @@ object OracleToDDL : ToDDL() {
         val hasPrimary = table.primaryKeyNames.isNotEmpty()
         val lastIndex = table.columns.size - 1
         table.columns.forEachIndexed { index, column ->
-            pw.println("  ${columnDef(column, quote)}${if (index < lastIndex || hasPrimary) "," else ""}")
+            pw.println(
+                "  ${
+                    columnDef(
+                        column,
+                        quote
+                    )
+                }${if (index < lastIndex || hasPrimary) "," else ""}"
+            )
         }
         appendKeys(table, hasPrimary, pw, quote, tableName, useForeignKey)
         pw.println(");")
