1e1c238f714e38eb80bfe31149d210c4b0765e7c
Peter Wu <piterwu@outlook.com>
2021-05-01 22:56:05 +0800

    优化微信配置

--- util/wechat/build.gradle.kts
+++ util/wechat/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
     api(project(":framework:web"))
     api("org.springframework:spring-tx")
 
-    api("cn.bestwu.wechat:weixin-mp")
+    compileOnly("cn.bestwu.wechat:weixin-mp")
     compileOnly("cn.bestwu.wechat:weixin-app")
 
     testImplementation(project(":util:test"))
--- util/wechat/src/main/java/cn/bestwu/summer/util/wechat/config/WechatConfiguration.java
+++ util/wechat/src/main/java/cn/bestwu/summer/util/wechat/config/WechatConfiguration.java
@@ -2,13 +2,18 @@ package cn.bestwu.summer.util.wechat.config;
 
 import cn.bestwu.summer.util.wechat.controller.WechatCallbackController;
 import cn.bestwu.summer.util.wechat.support.IWechatService;
+import com.riversoft.weixin.mp.base.AppSetting;
 import com.riversoft.weixin.mp.oauth2.MpOAuth2s;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnClass(AppSetting.class)
+@ConditionalOnProperty(prefix = "wechat", name = "appId")
 @Configuration
 @EnableConfigurationProperties({WechatProperties.class})
 public class WechatConfiguration {
