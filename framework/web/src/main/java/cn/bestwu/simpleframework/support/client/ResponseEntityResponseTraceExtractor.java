47864aa2dee109feaadb31f2634b4f45f1968a83
Peter Wu <piterwu@outlook.com>
2021-05-10 17:05:40 +0800

    ResponseEntityResponseTraceExtractor 调整

--- framework/web/src/main/java/cn/bestwu/simpleframework/support/client/ResponseEntityResponseTraceExtractor.java
+++ framework/web/src/main/java/cn/bestwu/simpleframework/support/client/ResponseEntityResponseTraceExtractor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.HttpMessageConverterExtractor;
@@ -15,7 +16,7 @@ import org.springframework.web.client.ResponseExtractor;
  * Response extractor for {@link HttpEntity}.
  */
 public class ResponseEntityResponseTraceExtractor<T> implements
-    ResponseExtractor<T> {
+    ResponseExtractor<ResponseEntity<T>> {
 
   private final Logger log = LoggerFactory.getLogger(this.getClass());
   private final HttpMessageConverterExtractor<T> delegate;
@@ -30,13 +31,15 @@ public class ResponseEntityResponseTraceExtractor<T> implements
   }
 
   @Override
-  public T extractData(ClientHttpResponse response) throws IOException {
+  public ResponseEntity<T> extractData(ClientHttpResponse response) throws IOException {
     if (this.delegate != null) {
       if (log.isInfoEnabled()) {
         response = new ClientHttpResponseWrapper(response);
         log.info("response:{}", ((ClientHttpResponseWrapper) response).getRecord());
       }
-      return this.delegate.extractData(response);
+      T body = this.delegate.extractData(response);
+      return ResponseEntity.status(response.getRawStatusCode()).headers(response.getHeaders())
+          .body(body);
     } else {
       return null;
     }
