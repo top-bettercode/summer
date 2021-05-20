73837b975cc3ec8663b1118302a33d1748cd1fa8
Peter Wu <piterwu@outlook.com>
2021-05-20 14:30:12 +0800

    BigDecimalSerializer增加精简小数配置

--- framework/web/src/main/java/cn/bestwu/simpleframework/web/serializer/BigDecimalSerializer.java
+++ framework/web/src/main/java/cn/bestwu/simpleframework/web/serializer/BigDecimalSerializer.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ser.std.NumberSerializer;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import org.springframework.util.StringUtils;
 
 @JacksonStdImpl
 public class BigDecimalSerializer extends NumberSerializer implements
@@ -19,15 +20,17 @@ public class BigDecimalSerializer extends NumberSerializer implements
   private static final long serialVersionUID = -6196337598040684558L;
   private final int scale;
   private final boolean toPlainString;
+  private final boolean reduceFraction;
 
   public BigDecimalSerializer() {
-    this(2, false);
+    this(2, false, false);
   }
 
-  public BigDecimalSerializer(int scale, boolean toPlainString) {
+  public BigDecimalSerializer(int scale, boolean toPlainString, boolean reduceFraction) {
     super(BigDecimal.class);
     this.scale = scale;
     this.toPlainString = toPlainString;
+    this.reduceFraction = reduceFraction;
   }
 
 
@@ -39,7 +42,13 @@ public class BigDecimalSerializer extends NumberSerializer implements
       content = content.setScale(scale, RoundingMode.HALF_UP);
     }
     if (toPlainString) {
-      gen.writeString(content.toPlainString());
+      String plainString = content.toPlainString();
+      if (reduceFraction) {
+        plainString = plainString.contains(".") ? StringUtils
+            .trimTrailingCharacter(StringUtils.trimTrailingCharacter(plainString, '0'), '.')
+            : plainString;
+      }
+      gen.writeString(plainString);
     } else {
       gen.writeNumber(content);
     }
@@ -52,7 +61,8 @@ public class BigDecimalSerializer extends NumberSerializer implements
       if (annotation == null) {
         throw new RuntimeException("未注解@" + JsonBigDecimal.class.getName());
       }
-      return new BigDecimalSerializer(annotation.scale(), annotation.toPlainString());
+      return new BigDecimalSerializer(annotation.scale(), annotation.toPlainString(),
+          annotation.reduceFraction());
     }
     return this;
   }
--- framework/web/src/test/java/cn/bestwu/simpleframework/web/serializer/CodeSerializerTest.java
+++ framework/web/src/test/java/cn/bestwu/simpleframework/web/serializer/CodeSerializerTest.java
@@ -1,14 +1,11 @@
 package cn.bestwu.simpleframework.web.serializer;
 
-import cn.bestwu.logging.operation.PrettyPrintingContentModifier;
 import cn.bestwu.simpleframework.support.code.ICodeService;
 import cn.bestwu.simpleframework.web.DataDicBean;
 import cn.bestwu.simpleframework.web.serializer.annotation.JsonCode;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -32,29 +29,6 @@ public class CodeSerializerTest {
       }
     });
 
-  }
-
-  @Test
-  public void serializeBigDecimal() throws Exception {
-    ObjectMapper objectMapper = new ObjectMapper();
-    objectMapper.setDefaultPropertyInclusion(Include.NON_NULL);
-
-    DataDicBean dicBean = new DataDicBean();
-    dicBean.setNumber1(new BigDecimal("1.234560"));
-    dicBean.setNumber2(new BigDecimal("1.23456"));
-    dicBean.setNumber3(new BigDecimal("1.10000"));
-    String string = objectMapper.writeValueAsString(dicBean);
-    System.err
-        .println(new String(PrettyPrintingContentModifier.INSTANCE.modifyContent(string.getBytes(
-            StandardCharsets.UTF_8))));
-    dicBean = new DataDicBean();
-    dicBean.setNumber1(new BigDecimal("1.234560"));
-    dicBean.setNumber2(new BigDecimal("1.20000"));
-    dicBean.setNumber3(new BigDecimal("1.010"));
-    string = objectMapper.writeValueAsString(dicBean);
-    System.err
-        .println(new String(PrettyPrintingContentModifier.INSTANCE.modifyContent(string.getBytes(
-            StandardCharsets.UTF_8))));
   }
 
   @Test
