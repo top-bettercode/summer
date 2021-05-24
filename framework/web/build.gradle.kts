1a5fe5bf0dd11ff666126371692a0622667990ff
Peter Wu <piterwu@outlook.com>
2021-05-24 14:19:33 +0800

    增加actuator

--- framework/web/build.gradle.kts
+++ framework/web/build.gradle.kts
@@ -5,11 +5,16 @@ dependencies {
     //web
     api("org.springframework.boot:spring-boot-starter-web")
 
+    //config
+    api("org.springframework.boot:spring-boot-starter-actuator")
+    api("org.springframework.cloud:spring-cloud-starter-config")
+
     compileOnly("com.github.axet:kaptcha")
     compileOnly("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
     compileOnly("org.springframework.boot:spring-boot-starter-jdbc")
 
     testImplementation("com.github.axet:kaptcha")
+    testImplementation(project(":util:test"))
 }
 
 
