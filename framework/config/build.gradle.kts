e85b2fdf7441f2e9684a22eda8ec02df2f92110a
Peter Wu <piterwu@outlook.com>
2021-06-02 17:14:51 +0800

    增加 WritableEnvironmentEndpoint

--- framework/config/build.gradle.kts
+++ framework/config/build.gradle.kts
@@ -3,4 +3,7 @@ plugins { `java-library` }
 dependencies {
     api("org.springframework.boot:spring-boot")
     api("org.slf4j:slf4j-api")
+    api("org.springframework.boot:spring-boot-starter-actuator")
+
+    compileOnly("org.springframework.cloud:spring-cloud-starter-config")
 }
