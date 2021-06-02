918ee6d3fb3e901e821e4b61a0c69df11c9d0c6b
Peter Wu <piterwu@outlook.com>
2021-06-02 14:41:25 +0800

    安全依赖配置

--- framework/security-server/src/main/java/cn/bestwu/simpleframework/security/server/SecurityServerConfiguration.java
+++ framework/security-server/src/main/java/cn/bestwu/simpleframework/security/server/SecurityServerConfiguration.java
@@ -1,17 +1,12 @@
 package cn.bestwu.simpleframework.security.server;
 
-import java.security.KeyPair;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,18 +15,14 @@ import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.rsa.crypto.KeyStoreKeyFactory;
 
 @Deprecated
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
-@EnableConfigurationProperties(KeyStoreProperties.class)
 public class SecurityServerConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
   public final MessageSource messageSource;
   private final UserDetailsService userDetailsService;
-
 
   public SecurityServerConfiguration(MessageSource messageSource,
       UserDetailsService userDetailsService) {
@@ -49,19 +40,6 @@ public class SecurityServerConfiguration extends GlobalAuthenticationConfigurerA
   public void init(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService);
   }
-
-  @ConditionalOnClass(KeyStoreKeyFactory.class)
-  @ConditionalOnProperty(prefix = "summer.security.key-store", value = "resource-path")
-  @Bean
-  public JwtAccessTokenConverter jwtAccessTokenConverter(KeyStoreProperties keyStoreProperties) {
-    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-    KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource(
-        keyStoreProperties.getResourcePath()), keyStoreProperties.getPassword().toCharArray())
-        .getKeyPair(keyStoreProperties.getAlias());
-    converter.setKeyPair(keyPair);
-    return converter;
-  }
-
 
   @Bean
   public SecurityOAuth2ErrorHandler securityOAuth2ErrorHandler(MessageSource messageSource,
