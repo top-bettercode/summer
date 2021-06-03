378c153132add7a18fd090291a4edf915acb6c80
Peter Wu <piterwu@outlook.com>
2021-06-03 16:54:43 +0800

    仓库配置

--- plugin/project-plugin/src/main/kotlin/plugin/ProjectPlugin.kt
+++ plugin/project-plugin/src/main/kotlin/plugin/ProjectPlugin.kt
@@ -70,23 +70,22 @@ class ProjectPlugin : Plugin<Project> {
                 java.targetCompatibility = JavaVersion.VERSION_1_8
             }
 
-            subProject.repositories.apply {
-                mavenLocal()
-                maven { it.setUrl("https://maven.aliyun.com/repository/public") }
-                mavenCentral()
-                val repositories = (project.findProperty("repositories") as? String
-                    ?: "http://maven.wintruelife.com/nexus/content/repositories/snapshots").split(",")
-                    .filter { it.isNotBlank() }
-
-                repositories.forEach { repository ->
-                    maven {
-                        it.setUrl(repository)
-                        it.isAllowInsecureProtocol = repository.startsWith("http://")
-                    }
-                }
-
-                maven { it.setUrl("https://oss.jfrog.org/oss-snapshot-local") }
-            }
+//            subProject.repositories.apply {
+//                mavenLocal()
+//                maven { it.setUrl("https://maven.aliyun.com/repository/public") }
+//                mavenCentral()
+//                val repositories = (project.findProperty("repositories") as? String)?.split(",")
+//                    ?.filter { it.isNotBlank() }
+//
+//                repositories?.forEach { repository ->
+//                    maven {
+//                        it.setUrl(repository)
+//                        it.isAllowInsecureProtocol = repository.startsWith("http://")
+//                    }
+//                }
+//
+//                maven { it.setUrl("https://oss.jfrog.org/oss-snapshot-local") }
+//            }
 
             val mainProject =
                 !arrayOf("core").contains(subProject.name) && subProject.parent?.name != "util" && subProject.name != "util"
