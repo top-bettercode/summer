7eeccc6089dedf61daffa7602c2b47b8ed52ae62
Peter Wu <piterwu@outlook.com>
2021-06-08 13:45:41 +0800

    仓库配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,15 +37,11 @@ allprojects {
 
     repositories {
         mavenLocal()
-        maven {
-            credentials {
-                username = "60b89f54395ee4198d8c67cf"
-                password = "WnLfTlxXBq(k"
-            }
-            setUrl("https://packages.aliyun.com/maven/repository/2021488-snapshot-4ZYq5w/")
-        }
-        gradlePluginPortal()
+        maven("https://maven.aliyun.com/repository/public/")
+        maven("https://maven.aliyun.com/repository/gradle-plugin/")
+        maven("https://s01.oss.sonatype.org/content/groups/public/")
         mavenCentral()
+        gradlePluginPortal()
     }
 
     configurations {
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,7 +1,8 @@
 pluginManagement {
     repositories {
         mavenLocal()
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/gradle-plugin/")
         gradlePluginPortal()
+        mavenCentral()
     }
 }
