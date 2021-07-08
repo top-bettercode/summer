95dc1c8154db4aa9c9a606c043e0da757b46557e
Peter Wu <piterwu@outlook.com>
2021-07-08 17:59:38 +0800

    实时日志优化

--- framework/starter-logging/src/main/kotlin/top/bettercode/logging/LogsEndpoint.kt
+++ framework/starter-logging/src/main/kotlin/top/bettercode/logging/LogsEndpoint.kt
@@ -99,6 +99,51 @@ class LogsEndpoint(
                     writer.println(
                         """
 <script type="text/javascript">
+  
+  function getScrollTop() {
+    var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+    if (document.body) {
+      bodyScrollTop = document.body.scrollTop;
+    }
+    if (document.documentElement) {
+      documentScrollTop = document.documentElement.scrollTop;
+    }
+    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+    return scrollTop;
+  }
+
+  //文档的总高度
+
+  function getScrollHeight() {
+    var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+    if (document.body) {
+      bodyScrollHeight = document.body.scrollHeight;
+    }
+    if (document.documentElement) {
+      documentScrollHeight = document.documentElement.scrollHeight;
+    }
+    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight
+        : documentScrollHeight;
+    return scrollHeight;
+  }
+
+  //浏览器视口的高度
+
+  function getWindowHeight() {
+    var windowHeight = 0;
+    if (document.compatMode === "CSS1Compat") {
+      windowHeight = document.documentElement.clientHeight;
+    } else {
+      windowHeight = document.body.clientHeight;
+    }
+    return windowHeight;
+  }
+  
+  document.onEnd = true
+  window.onscroll = function () {
+    document.onEnd = getScrollTop() + getWindowHeight() === getScrollHeight();
+  };
+  
   //websocket对象
   let websocket = null;
   //判断当前浏览器是否支持WebSocket
