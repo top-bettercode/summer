34665641854221a47c9692c98463ede0179326fe
Peter Wu <piterwu@outlook.com>
2021-07-29 14:08:02 +0800

    日志文件顺序调整

--- framework/config/src/main/kotlin/top/bettercode/config/LogFileNameComparator.kt
+++ framework/config/src/main/kotlin/top/bettercode/config/LogFileNameComparator.kt
@@ -11,25 +11,23 @@ class LogFileNameComparator : Comparator<File> {
     override fun compare(o1: File, o2: File): Int {
         val compareTo = o1.isFile.compareTo(o2.isFile)
         return if (compareTo == 0) {
-            if (o1.isDirectory || o1.name.contains("+")) {
+            if (o1.isDirectory) {
                 o1.name.compareTo(o2.name)
             } else {
                 try {
                     val name1 = o1.nameWithoutExtension
                     val name2 = o2.nameWithoutExtension
-                    val compareTo1 = name1.contains("-").compareTo(name2.contains("-"))
+                    val a1 =
+                        if (name1.contains("-")) name1.substringBeforeLast("-") else name1 + "zzzz"
+                    val b1 = if (name1.contains("-")) name1.substringAfterLast("-")
+                        .toInt() else Int.MAX_VALUE
+                    val a2 =
+                        if (name2.contains("-")) name2.substringBeforeLast("-") else name2 + "zzzz"
+                    val b2 = if (name2.contains("-")) name2.substringAfterLast("-")
+                        .toInt() else Int.MAX_VALUE
+                    val compareTo1 = a2.compareTo(a1)
                     if (compareTo1 == 0) {
-                        if (name1.contains("-")) {
-                            val compareTo2 = name1.substringBeforeLast("-")
-                                .compareTo(name2.substringBeforeLast("-"))
-                            if (compareTo2 == 0) {
-                                name1.substringAfterLast("-").toInt()
-                                    .compareTo(name2.substringAfterLast("-").toInt())
-                            } else
-                                compareTo2
-                        } else {
-                            name1.compareTo(name2)
-                        }
+                        b2.compareTo(b1)
                     } else
                         compareTo1
                 } catch (e: Exception) {
