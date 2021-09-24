4a6d8405de16e8b220a7dbc414a5c5b2833200f3
Peter Wu <piterwu@outlook.com>
2021-09-24 11:02:58 +0800

    mybatis异常BUG修复

--- framework/data-mybatis/src/main/java/top/bettercode/simpleframework/data/IbatisErrorHandler.java
+++ framework/data-mybatis/src/main/java/top/bettercode/simpleframework/data/IbatisErrorHandler.java
@@ -1,13 +1,13 @@
 package top.bettercode.simpleframework.data;
 
-import top.bettercode.simpleframework.web.RespEntity;
-import top.bettercode.simpleframework.web.error.AbstractErrorHandler;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.ibatis.exceptions.PersistenceException;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
+import top.bettercode.simpleframework.web.RespEntity;
+import top.bettercode.simpleframework.web.error.AbstractErrorHandler;
 
 /**
  * @author Peter Wu
@@ -25,14 +25,20 @@ public class IbatisErrorHandler extends AbstractErrorHandler {
       Map<String, String> errors, String separator) {
     String message = null;
     if (error instanceof PersistenceException) {
-      message = error.getCause().getMessage().trim();
+      Throwable cause = error.getCause();
+      if (cause != null) {
+        String causeMessage = cause.getMessage();
+        if (causeMessage != null) {
+          message = causeMessage.trim();
 //ORA-12899: 列 "YUNTUDEV"."PU_ASK_SEND_TMS"."FROM_ADDRESS" 的值太大 (实际值: 1421, 最大值: 600)
-      String regex = "ORA-12899: .*\\..*\\.\"(.*?)\" 的值太大 \\(实际值: \\d+, 最大值: (\\d+)\\)";
-      if (message.matches(regex)) {
-        String field = message.replaceAll(regex, "$1");
-        String maxLeng = message.replaceAll(regex, "$2");
-        message = getText(field) + "长度不能大于" + maxLeng;
-        respEntity.setHttpStatusCode(HttpStatus.BAD_REQUEST.value());
+          String regex = "ORA-12899: .*\\..*\\.\"(.*?)\" 的值太大 \\(实际值: \\d+, 最大值: (\\d+)\\)";
+          if (message.matches(regex)) {
+            String field = message.replaceAll(regex, "$1");
+            String maxLeng = message.replaceAll(regex, "$2");
+            message = getText(field) + "长度不能大于" + maxLeng;
+            respEntity.setHttpStatusCode(HttpStatus.BAD_REQUEST.value());
+          }
+        }
       }
     }
     if (StringUtils.hasText(message)) {
