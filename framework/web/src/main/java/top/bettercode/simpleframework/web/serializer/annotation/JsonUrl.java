d1561c37570e7503583c9f89f9a48dc42bdd5d6e
Peter Wu <piterwu@outlook.com>
2021-10-11 15:15:22 +0800

    json序列化优化

--- framework/web/src/main/java/top/bettercode/simpleframework/web/serializer/annotation/JsonUrl.java
+++ framework/web/src/main/java/top/bettercode/simpleframework/web/serializer/annotation/JsonUrl.java
@@ -1,20 +1,22 @@
 package top.bettercode.simpleframework.web.serializer.annotation;
 
-import top.bettercode.simpleframework.web.serializer.JsonUrlMapper;
-import top.bettercode.simpleframework.web.serializer.UrlSerializer;
 import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import top.bettercode.simpleframework.web.serializer.JsonUrlMapper;
+import top.bettercode.simpleframework.web.serializer.UrlSerializer;
 
 /**
  * @author Peter Wu
  */
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @JacksonAnnotationsInside
+@Inherited
 @JsonSerialize(using = UrlSerializer.class)
 public @interface JsonUrl {
 
