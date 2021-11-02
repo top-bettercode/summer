5ada9b324b3586556bf13f00008fba5b48117a62
Peter Wu <piterwu@outlook.com>
2021-11-02 11:13:06 +0800

    增加版本比较

--- util/common-lang/src/main/kotlin/top/bettercode/lang/util/StringUtil.kt
+++ util/common-lang/src/main/kotlin/top/bettercode/lang/util/StringUtil.kt
@@ -383,4 +383,64 @@ object StringUtil {
             ), '.'
         ) else value
     }
+
+    internal val versionTails = arrayOf("SNAPSHOTS", "ALPHA", "BETA", "M", "RC", "RELEASE")
+    internal val versionTailRegex = "^([A-Za-z]+?)(\\d*)$".toRegex()
+
+    /**
+     * 比较版本信息
+
+     * @param version1 版本1
+     * *
+     * @param version2 版本2
+     * *
+     * @return int
+     */
+    @JvmStatic
+    fun compareVersion(version1: String, version2: String): Int {
+        if (version1 == version2) {
+            return 0
+        }
+        val separator = "[.-]"
+        val version1s = version1.split(separator.toRegex()).toMutableList()
+        val version2s = version2.split(separator.toRegex()).toMutableList()
+
+        if (version1s.size < version2s.size) {
+            version1s.addAll(List(version2s.size - version1s.size) { "" })
+        } else {
+            version2s.addAll(List(version1s.size - version2s.size) { "" })
+        }
+        val length = version1s.size
+
+        for (i in 0 until length) {
+            val toIntOrNull2 = version2s[i].toIntOrNull()
+            val toIntOrNull1 = version1s[i].toIntOrNull()
+            if (toIntOrNull1 == null && toIntOrNull2 != null)
+                return -1
+            else if (toIntOrNull1 != null && toIntOrNull2 == null)
+                return 1
+            var v2 = toIntOrNull2
+                ?: versionTails.indexOf(version2s[i].replace(versionTailRegex, "$1").toUpperCase())
+            var v1 = toIntOrNull1
+                ?: versionTails.indexOf(version1s[i].replace(versionTailRegex, "$1").toUpperCase())
+            if (v1 != -1 && v1 == v2 && toIntOrNull1 == null && toIntOrNull2 == null) {
+                v2 = version2s[i].replace(versionTailRegex, "$2").toIntOrNull() ?: 0
+                v1 = version1s[i].replace(versionTailRegex, "$2").toIntOrNull() ?: 0
+            }
+            if (v1 == -1 || v2 == -1) {
+                val result = version1s[i].compareTo(version2s[i])
+                if (result != 0) {
+                    return result
+                }
+            }
+            if (v2 > v1) {
+                return -1
+            } else if (v2 < v1) {
+                return 1
+            }
+            // 相等 比较下一组值
+        }
+        return 0
+    }
+
 }
