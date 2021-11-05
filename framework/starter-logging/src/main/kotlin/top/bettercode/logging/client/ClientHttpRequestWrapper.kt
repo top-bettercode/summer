c918aa88d8759e91ef351cd42531342ede729a24
Peter Wu <piterwu@outlook.com>
2021-11-05 13:43:39 +0800

    接口请求日志优化

--- framework/starter-logging/src/main/kotlin/top/bettercode/logging/client/ClientHttpRequestWrapper.kt
+++ framework/starter-logging/src/main/kotlin/top/bettercode/logging/client/ClientHttpRequestWrapper.kt
@@ -1,10 +1,5 @@
 package top.bettercode.logging.client
 
-import top.bettercode.logging.RequestLoggingConfig
-import top.bettercode.logging.operation.Operation
-import top.bettercode.logging.operation.OperationResponse
-import top.bettercode.logging.operation.RequestConverter
-import top.bettercode.logging.operation.ResponseConverter
 import org.slf4j.LoggerFactory
 import org.slf4j.MarkerFactory
 import org.springframework.http.HttpHeaders
@@ -12,6 +7,12 @@ import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.client.ClientHttpRequest
 import org.springframework.http.client.ClientHttpResponse
+import top.bettercode.lang.util.StringUtil
+import top.bettercode.logging.RequestLoggingConfig
+import top.bettercode.logging.operation.Operation
+import top.bettercode.logging.operation.OperationResponse
+import top.bettercode.logging.operation.RequestConverter
+import top.bettercode.logging.operation.ResponseConverter
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.OutputStream
@@ -37,54 +38,52 @@ class ClientHttpRequestWrapper(
                 LocalDateTime.now()
             } else null
         var response: ClientHttpResponse? = null
+        var stackTrace = ""
         try {
             response = request.execute()
             if (log.isInfoEnabled)
                 response = ClientHttpResponseWrapper(response)
             return response!!
+        } catch (e: Exception) {
+            stackTrace = StringUtil.valueOf(e)
+            throw e
         } finally {
             if (log.isInfoEnabled) {
+                val operationResponse = if (response == null) OperationResponse(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    HttpHeaders.EMPTY, ByteArray(0)
+                ) else ResponseConverter.convert(response as ClientHttpResponseWrapper)
+                operationResponse.stackTrace = stackTrace
                 val operation = Operation(
                     collectionName = collectionName,
                     name = name,
                     protocol = "HTTP/1.1",
                     request = RequestConverter.convert(this, dateTime!!),
-                    response = if (response == null) OperationResponse(
-                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                        HttpHeaders.EMPTY, ByteArray(0)
-                    ) else ResponseConverter.convert(response as ClientHttpResponseWrapper)
+                    response = operationResponse
                 )
+                val msg = operation.toString(
+                    RequestLoggingConfig(
+                        includeRequestBody = true,
+                        includeResponseBody = true,
+                        includeTrace = true,
+                        encryptHeaders = arrayOf(),
+                        encryptParameters = arrayOf(),
+                        format = true,
+                        ignoredTimeout = true,
+                        timeoutAlarmSeconds = -1
+                    )
+                )
+                val hasException = stackTrace.isNotBlank()
                 if (logMarker.isNullOrBlank()) {
-                    log.info(
-                        operation.toString(
-                            RequestLoggingConfig(
-                                includeRequestBody = true,
-                                includeResponseBody = true,
-                                includeTrace = true,
-                                encryptHeaders = arrayOf(),
-                                encryptParameters = arrayOf(),
-                                format = true,
-                                ignoredTimeout = true,
-                                timeoutAlarmSeconds = -1
-                            )
-                        )
-                    )
+                    if (hasException) {
+                        log.warn(msg)
+                    } else
+                        log.info(msg)
                 } else {
-                    log.info(
-                        MarkerFactory.getMarker(logMarker),
-                        operation.toString(
-                            RequestLoggingConfig(
-                                includeRequestBody = true,
-                                includeResponseBody = true,
-                                includeTrace = true,
-                                encryptHeaders = arrayOf(),
-                                encryptParameters = arrayOf(),
-                                format = true,
-                                ignoredTimeout = true,
-                                timeoutAlarmSeconds = -1
-                            )
-                        )
-                    )
+                    if (hasException) {
+                        log.warn(MarkerFactory.getMarker(logMarker), msg)
+                    } else
+                        log.info(MarkerFactory.getMarker(logMarker), msg)
                 }
             }
         }
