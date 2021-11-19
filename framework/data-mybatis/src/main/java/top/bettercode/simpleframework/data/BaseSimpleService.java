7fff769ba03a562af088175ea6790fa6ff33fcff
Peter Wu <piterwu@outlook.com>
2021-11-19 15:04:42 +0800

    BaseSimpleService 优化

--- framework/data-mybatis/src/main/java/top/bettercode/simpleframework/data/BaseSimpleService.java
+++ framework/data-mybatis/src/main/java/top/bettercode/simpleframework/data/BaseSimpleService.java
@@ -1,11 +1,15 @@
 package top.bettercode.simpleframework.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author Peter Wu
  */
 public class BaseSimpleService<M> {
+
+  protected Logger log = LoggerFactory.getLogger(getClass());
 
   @Autowired
   protected M baseMapper;
