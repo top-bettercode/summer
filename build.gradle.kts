1d11d532b5d6199247858c66365934092006a714
Peter Wu <piterwu@outlook.com>
2021-12-13 14:15:53 +0800

    log4j日志升级

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,6 +117,8 @@ allprojects {
 
             dependency("jakarta.persistence:jakarta.persistence-api:2.2.3")
             //-
+            dependency("org.apache.logging.log4j:log4j-api:2.15.0")
+            dependency("org.apache.logging.log4j:log4j-core:2.15.0")
 
         }
     }
