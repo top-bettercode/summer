7ac03077d96bad365bc0e4dda959c100d9bc534d
Peter Wu <piterwu@outlook.com>
2021-12-28 10:47:20 +0800

    微信工具事务配置

--- util/wechat/src/main/java/top/bettercode/summer/util/wechat/support/IWechatService.java
+++ util/wechat/src/main/java/top/bettercode/summer/util/wechat/support/IWechatService.java
@@ -1,12 +1,9 @@
 package top.bettercode.summer.util.wechat.support;
 
-import org.springframework.transaction.annotation.Transactional;
-
 /**
  * @author Peter Wu
  */
 public interface IWechatService {
 
-  @Transactional
   String oauth(String openId);
 }
