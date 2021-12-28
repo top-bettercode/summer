c3ca21aa9cb5b099e22ad0ee40669057e5c946cd
Peter Wu <piterwu@outlook.com>
2021-12-28 13:45:29 +0800

    data-jpa 异常处理

--- framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/config/DataJpaConfiguration.java
+++ framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/config/DataJpaConfiguration.java
@@ -1,9 +1,13 @@
 package top.bettercode.simpleframework.data.jpa.config;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import top.bettercode.simpleframework.data.jpa.IbatisErrorHandler;
 
 /**
  * DataJpaConfiguration 配置
@@ -19,4 +23,12 @@ public class DataJpaConfiguration {
   public JpaExtProperties jpaExtProperties() {
     return new JpaExtProperties();
   }
+
+  @ConditionalOnWebApplication
+  @Bean
+  public IbatisErrorHandler ibatisErrorHandler(MessageSource messageSource,
+      HttpServletRequest request) {
+    return new IbatisErrorHandler(messageSource, request);
+  }
+
 }
