c9b2d1f070a6e724801f231f0e3cbb5f18d6569f
Peter Wu <piterwu@outlook.com>
2021-12-30 09:52:43 +0800

    增加 ClientAuthorize

--- framework/security/src/main/java/top/bettercode/simpleframework/security/URLFilterInvocationSecurityMetadataSource.java
+++ framework/security/src/main/java/top/bettercode/simpleframework/security/URLFilterInvocationSecurityMetadataSource.java
@@ -30,6 +30,7 @@ import top.bettercode.simpleframework.security.config.ApiSecurityProperties;
 public class URLFilterInvocationSecurityMetadataSource implements
     FilterInvocationSecurityMetadataSource {
 
+  private static final List<AntPathRequestMatcher> clientAuthorizes = new ArrayList<>();
   private final Map<AntPathRequestMatcher, Set<ConfigAttribute>> defaultConfigAttributes = new HashMap<>();
   private Map<AntPathRequestMatcher, Set<ConfigAttribute>> requestMatcherConfigAttributes;
   private final IResourceService securityService;
@@ -69,8 +70,30 @@ public class URLFilterInvocationSecurityMetadataSource implements
           }
         }
       }
+      //ClientAuthorize
+      if (AnnotatedUtils.hasAnnotation(handlerMethod, ClientAuthorize.class)) {
+        for (String pattern : mappingInfo.getPatternsCondition().getPatterns()) {
+          Set<RequestMethod> methods = mappingInfo.getMethodsCondition().getMethods();
+          if (methods.isEmpty()) {
+            clientAuthorizes.add(new AntPathRequestMatcher(pattern));
+          } else {
+            for (RequestMethod requestMethod : methods) {
+              clientAuthorizes.add(new AntPathRequestMatcher(pattern, requestMethod.name()));
+            }
+          }
+        }
+      }
     });
     bindConfigAttributes();
+  }
+
+  public static boolean matchClientAuthorize(HttpServletRequest request) {
+    for (AntPathRequestMatcher clientAuthorize : clientAuthorizes) {
+      if (clientAuthorize.matches(request)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /**
