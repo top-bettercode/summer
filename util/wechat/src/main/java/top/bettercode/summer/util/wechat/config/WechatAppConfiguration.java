6ad6a861e47b92abeb05167466f835e5ecf950b4
Peter Wu <piterwu@outlook.com>
2021-12-30 16:48:15 +0800

    微信配置

--- util/wechat/src/main/java/top/bettercode/summer/util/wechat/config/WechatAppConfiguration.java
+++ util/wechat/src/main/java/top/bettercode/summer/util/wechat/config/WechatAppConfiguration.java
@@ -1,19 +1,21 @@
 package top.bettercode.summer.util.wechat.config;
 
-import top.bettercode.summer.util.wechat.controller.WechatAppCallbackController;
-import top.bettercode.summer.util.wechat.support.IWechatService;
 import com.riversoft.weixin.app.base.AppSetting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import top.bettercode.summer.util.wechat.controller.WechatAppCallbackController;
+import top.bettercode.summer.util.wechat.support.IWechatService;
 
 @ConditionalOnClass(AppSetting.class)
 @ConditionalOnProperty(prefix = "summer.wechat.mini", name = "app-id")
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication
 @EnableConfigurationProperties({WechatMiniAppProperties.class})
 public class WechatAppConfiguration {
 
