f6562d6dfcb8637ebb5072dc6de4c56df6bec46a
Peter Wu <piterwu@outlook.com>
2021-12-31 16:13:55 +0800

    依赖版本调整

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
 
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.0")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.3.12.RELEASE")
 
     implementation("top.bettercode.summer:publish-plugin:0.0.13-SNAPSHOT")
 //    implementation("top.bettercode.summer:publish-plugin:$summerVersion")
