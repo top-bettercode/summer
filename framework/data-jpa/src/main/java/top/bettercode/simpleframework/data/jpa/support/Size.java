09b5a00cbb2c525e36078791ffc2097bf1d68016
Peter Wu <piterwu@outlook.com>
2022-02-26 12:39:53 +0800

    mybatis 参数及分页信息解析优化

---
+++ framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/Size.java
@@ -0,0 +1,32 @@
+package top.bettercode.simpleframework.data.jpa.support;
+
+import org.springframework.data.domain.Sort;
+
+/**
+ * @author Peter Wu
+ */
+public class Size {
+  private final int size;
+  private final Sort sort;
+
+  public static Size of(int size) {
+    return of(size, Sort.unsorted());
+  }
+
+  public static Size of(int size, Sort sort) {
+    return new Size(size, sort);
+  }
+
+  private Size(int size, Sort sort) {
+    this.size = size;
+    this.sort = sort;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public Sort getSort() {
+    return sort;
+  }
+}
