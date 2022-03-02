b62e677d70bd7c4628d28cf5d5400ba131b8bacd
Peter Wu <piterwu@outlook.com>
2022-03-02 20:09:38 +0800

    优化日志列表排序

--- framework/config/src/main/kotlin/top/bettercode/config/LogFileNameComparator.kt
+++ framework/config/src/main/kotlin/top/bettercode/config/LogFileNameComparator.kt
@@ -15,32 +15,38 @@ class LogFileNameComparator : Comparator<File> {
                 o1.name.compareTo(o2.name)
             } else {
                 try {
-                    val name1 = o1.nameWithoutExtension
-                    val name2 = o2.nameWithoutExtension
-                    val contains1 = name1.contains("-")
-                    val a1 =
-                        if (contains1) name1.substringBefore("-") else name1.substringBefore(
-                            "."
-                        )
-                    val contains2 = name2.contains("-")
-                    val a2 =
-                        if (contains2) name2.substringBefore("-") else name2.substringBefore(
-                            "."
-                        )
-                    val compareTo1 = a1.compareTo(a2)
-                    if (compareTo1 == 0) {
-                        val compareTo2 = o2.lastModified().compareTo(o1.lastModified())
-                        if (compareTo2 == 0) {
-                            contains1.compareTo(contains2)
-                        } else
-                            compareTo2
-                    } else
-                        compareTo1
+                    val name1 = o1.name.split('-', ':', '.')
+                    val name2 = o2.name.split('-', ':', '.')
+                    val size1 = name1.size
+                    val size2 = name2.size
+                    if (size1 > size2)
+                        name1.forEachIndexed { i, s1 ->
+                            if (i >= size2) return 1
+                            val s2 = name2[i]
+                            val compareTo1 = compareStr(s1, s2)
+                            if (compareTo1 != 0)
+                                return compareTo1
+                        }
+                    else
+                        name2.forEachIndexed { i, s2 ->
+                            if (i >= size1) return -1
+                            val s1 = name1[i]
+                            val compareTo1 = compareStr(s1, s2)
+                            if (compareTo1 != 0)
+                                return compareTo1
+                        }
+                    0
                 } catch (e: Exception) {
                     o1.name.compareTo(o2.name)
                 }
             }
         } else
             compareTo
+    }
+
+    private fun compareStr(s1: String, s2: String): Int {
+        val n1 = s1.toIntOrNull()
+        val n2 = s2.toIntOrNull()
+        return if (n1 != null && n2 != null) n2.compareTo(n1) else s1.compareTo(s2)
     }
 }
