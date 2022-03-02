eaeb66d4b8191e537f0521eebbac033692c61c52
Peter Wu <piterwu@outlook.com>
2022-03-02 20:09:38 +0800

    优化日志列表排序

--- framework/config/src/test/kotlin/top/bettercode/config/LogFileNameComparatorTest.kt
+++ framework/config/src/test/kotlin/top/bettercode/config/LogFileNameComparatorTest.kt
@@ -8,15 +8,34 @@ import java.io.File
  *
  * @author Peter Wu
  */
-internal class LogFileNameComparatorTest {
+class LogFileNameComparatorTest {
 
     @Test
-    internal fun comparator() {
+    fun comparator() {
         val listFiles =
-            File("/data/repositories/bettercode/wintruelife/template/app/build/logs").listFiles()
-        listFiles?.sortWith(LogFileNameComparator())
-        listFiles?.forEach {
+            arrayOf(
+                File("/alarm/2022-03-02-10:20:54.673-error-24760.log#last"),
+                File("/alarm/2022-03-02-10:21:54.673-error-24760.log#last"),
+                File("/alarm/2021-12-31-15:31:51.539-ERROR-21572.log#last"),
+                File("all.log-2022-03-02-2.gz"),
+                File("all.log-2022-03-02-3.gz"),
+                File("all.log-2022-03-02-4.gz"),
+                File("all.log"),
+                File("root.log-2022-03-02-4.gz"),
+                File("root.log")
+            )
+        listFiles.sortWith(LogFileNameComparator())
+        listFiles.forEach {
             println(it.name)
         }
+    }
+
+    @Test
+    fun compare() {
+        val compare = LogFileNameComparator().compare(
+            File("all.log-2022-03-02-4.gz"),
+            File("all.log")
+        )
+        System.err.println(compare)
     }
 }
