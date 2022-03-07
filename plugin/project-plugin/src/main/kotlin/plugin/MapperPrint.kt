fe7715bd0cd089652015cc8f0a8a5fe52201208c
Peter Wu <piterwu@outlook.com>
2022-03-07 14:22:20 +0800

    printMapper优化

--- plugin/project-plugin/src/main/kotlin/plugin/MapperPrint.kt
+++ plugin/project-plugin/src/main/kotlin/plugin/MapperPrint.kt
@@ -9,7 +9,14 @@ open class MapperPrint : ProjectGenerator() {
 
     override fun call() {
         println("""<resultMap type="${entityType.fullyQualifiedNameWithoutTypeParameters}" id="${entityName}Map">""")
-        columns.forEach {
+        if (isCompositePrimaryKey) {
+            primaryKeys.forEach {
+                println("""    <result property="${primaryKeyName}.${it.javaName}" column="${it.columnName}" />""")
+            }
+        } else {
+            println("    <result property=\"${primaryKeyName}\" column=\"${primaryKey.columnName}\"/>")
+        }
+        otherColumns.forEach {
             println("    <result property=\"${it.javaName}\" column=\"${it.columnName}\"/>")
         }
         println("""  </resultMap>""")
