38ab9daff93be8cf0df17935f4ec5ad8e530e7ea
Peter Wu <piterwu@outlook.com>
2022-03-22 20:47:09 +0800

    mybatis 类型转换调整

--- framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/JpaUtil.java
+++ framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/JpaUtil.java
@@ -1,49 +1,26 @@
 package top.bettercode.simpleframework.data.jpa.support;
 
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.convert.support.DefaultConversionService;
+import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
+import org.hibernate.type.descriptor.java.spi.JavaTypeDescriptorRegistry;
+import org.hibernate.type.spi.TypeConfiguration;
+import org.springframework.util.ClassUtils;
 
 /**
  * @author Peter Wu
  */
 public class JpaUtil {
 
-  private static final DefaultConversionService CONVERSION_SERVICE = new DefaultConversionService();
-
-  static {
-    CONVERSION_SERVICE.addConverter(new Converter<Timestamp, LocalDate>() {
-      @Override
-      public LocalDate convert(Timestamp source) {
-        return source.toLocalDateTime().toLocalDate();
-      }
-    });
-    CONVERSION_SERVICE.addConverter(new Converter<Timestamp, LocalDateTime>() {
-      @Override
-      public LocalDateTime convert(Timestamp source) {
-        return source.toLocalDateTime();
-      }
-    });
-    CONVERSION_SERVICE.addConverter(new Converter<Boolean, Integer>() {
-      @Override
-      public Integer convert(Boolean source) {
-        return source ? 1 : 0;
-      }
-    });
-  }
+  private static final JavaTypeDescriptorRegistry TYPE_DESCRIPTOR_REGISTRY = new TypeConfiguration().getJavaTypeDescriptorRegistry();
 
   @SuppressWarnings("unchecked")
   public static <T> T convert(Object source, Class<T> targetType) {
     if (source != null && !targetType.isInstance(source)) {
-      return JpaUtil.CONVERSION_SERVICE.convert(source, targetType);
+      JavaTypeDescriptor<T> descriptor = (JavaTypeDescriptor<T>) TYPE_DESCRIPTOR_REGISTRY.getDescriptor(
+          ClassUtils.resolvePrimitiveIfNecessary(targetType));
+      return descriptor.wrap(source, null);
     } else {
       return (T) source;
     }
   }
 
-  public static boolean canConvert(Class<?> sourceType, Class<?> targetType) {
-    return CONVERSION_SERVICE.canConvert(sourceType, targetType);
-  }
 }
