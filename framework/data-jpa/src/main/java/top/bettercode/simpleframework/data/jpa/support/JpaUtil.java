f7f6017be2281d9a2d474fe3e554741cb8ef3104
Peter Wu <piterwu@outlook.com>
2022-03-23 23:02:01 +0800

    代码优化

--- framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/JpaUtil.java
+++ framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/JpaUtil.java
@@ -1,20 +1,19 @@
 package top.bettercode.simpleframework.data.jpa.support;
 
 import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
-import org.hibernate.type.descriptor.java.spi.JavaTypeDescriptorRegistry;
-import org.hibernate.type.spi.TypeConfiguration;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.util.ClassUtils;
 
 /**
  * @author Peter Wu
  */
+@SuppressWarnings("deprecation")
 public class JpaUtil {
 
-  private static final JavaTypeDescriptorRegistry TYPE_DESCRIPTOR_REGISTRY = new TypeConfiguration().getJavaTypeDescriptorRegistry();
+  private static final org.hibernate.type.descriptor.java.JavaTypeDescriptorRegistry TYPE_DESCRIPTOR_REGISTRY = org.hibernate.type.descriptor.java.JavaTypeDescriptorRegistry.INSTANCE;
   private static final DefaultConversionService CONVERSION_SERVICE = new DefaultConversionService();
 
-  @SuppressWarnings({"unchecked","deprecation"})
+  @SuppressWarnings({"unchecked"})
   public static <T> T convert(Object source, Class<T> targetType) {
     if (source != null && !targetType.isInstance(source)) {
       JavaTypeDescriptor<T> descriptor = (JavaTypeDescriptor<T>) TYPE_DESCRIPTOR_REGISTRY.getDescriptor(
