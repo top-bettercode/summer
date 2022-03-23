1f9df8d5b0646bbca7ccdb7945eb37986ca18fa7
Peter Wu <piterwu@outlook.com>
2022-03-23 12:31:36 +0800

    mybatis 结果转换调整

--- framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/JpaUtil.java
+++ framework/data-jpa/src/main/java/top/bettercode/simpleframework/data/jpa/support/JpaUtil.java
@@ -3,6 +3,7 @@ package top.bettercode.simpleframework.data.jpa.support;
 import org.hibernate.type.descriptor.java.JavaTypeDescriptor;
 import org.hibernate.type.descriptor.java.spi.JavaTypeDescriptorRegistry;
 import org.hibernate.type.spi.TypeConfiguration;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.util.ClassUtils;
 
 /**
@@ -11,13 +12,18 @@ import org.springframework.util.ClassUtils;
 public class JpaUtil {
 
   private static final JavaTypeDescriptorRegistry TYPE_DESCRIPTOR_REGISTRY = new TypeConfiguration().getJavaTypeDescriptorRegistry();
+  private static final DefaultConversionService CONVERSION_SERVICE = new DefaultConversionService();
 
   @SuppressWarnings("unchecked")
   public static <T> T convert(Object source, Class<T> targetType) {
     if (source != null && !targetType.isInstance(source)) {
       JavaTypeDescriptor<T> descriptor = (JavaTypeDescriptor<T>) TYPE_DESCRIPTOR_REGISTRY.getDescriptor(
           ClassUtils.resolvePrimitiveIfNecessary(targetType));
-      return descriptor.wrap(source, null);
+      try {
+        return descriptor.wrap(source, null);
+      } catch (Exception e) {
+        return CONVERSION_SERVICE.convert(source, targetType);
+      }
     } else {
       return (T) source;
     }
