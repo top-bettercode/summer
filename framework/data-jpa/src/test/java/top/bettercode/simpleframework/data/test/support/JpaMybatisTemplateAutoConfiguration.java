631013234e7f32b5b296920bc880bdf092ec9bca
Peter Wu <piterwu@outlook.com>
2022-03-25 13:39:25 +0800

    代码优化

--- framework/data-jpa/src/test/java/top/bettercode/simpleframework/data/test/support/JpaMybatisTemplateAutoConfiguration.java
+++ framework/data-jpa/src/test/java/top/bettercode/simpleframework/data/test/support/JpaMybatisTemplateAutoConfiguration.java
@@ -1,17 +1,11 @@
 package top.bettercode.simpleframework.data.test.support;
 
 import javax.sql.DataSource;
-import org.apache.ibatis.mapping.DatabaseIdProvider;
-import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.type.TypeHandler;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -21,36 +15,21 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.Assert;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
+import top.bettercode.simpleframework.data.jpa.config.JpaMybatisAutoConfiguration;
 import top.bettercode.simpleframework.data.jpa.config.MybatisProperties;
 
 @org.springframework.context.annotation.Configuration(proxyBeanMethods = false)
 @AutoConfigureAfter({DataSourceAutoConfiguration.class})
 public class JpaMybatisTemplateAutoConfiguration implements InitializingBean {
 
-  private static final Logger logger = LoggerFactory
-      .getLogger(JpaMybatisTemplateAutoConfiguration.class);
-
   private final MybatisProperties properties;
-
-  private final Interceptor[] interceptors;
-
-  private final TypeHandler<?>[] typeHandlers;
-
   private final ResourceLoader resourceLoader;
 
-  private final DatabaseIdProvider databaseIdProvider;
 
-
-  @SuppressWarnings("rawtypes")
-  public JpaMybatisTemplateAutoConfiguration(
-      MybatisProperties properties, ObjectProvider<Interceptor[]> interceptorsProvider,
-      ObjectProvider<TypeHandler[]> typeHandlersProvider,
-      ResourceLoader resourceLoader, ObjectProvider<DatabaseIdProvider> databaseIdProvider) {
+  public JpaMybatisTemplateAutoConfiguration(MybatisProperties properties,
+      ResourceLoader resourceLoader) {
     this.properties = properties;
-    this.interceptors = interceptorsProvider.getIfAvailable();
-    this.typeHandlers = typeHandlersProvider.getIfAvailable();
     this.resourceLoader = resourceLoader;
-    this.databaseIdProvider = databaseIdProvider.getIfAvailable();
   }
 
   @Override
@@ -73,40 +52,23 @@ public class JpaMybatisTemplateAutoConfiguration implements InitializingBean {
   public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
     SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
     factory.setDataSource(dataSource);
-//    factory.setVfs(SpringBootVFS.class);
-    if (StringUtils.hasText(this.properties.getConfigLocation())) {
-      factory
-          .setConfigLocation(this.resourceLoader.getResource(this.properties.getConfigLocation()));
+    String configLocation = this.properties.getConfigLocation();
+    if (StringUtils.hasText(configLocation)) {
+      factory.setConfigLocation(this.resourceLoader.getResource(configLocation));
     }
     Configuration configuration = this.properties.getConfiguration();
-    if (configuration == null && !StringUtils.hasText(this.properties.getConfigLocation())) {
-      configuration = new Configuration();
-    }
     factory.setConfiguration(configuration);
-    if (this.properties.getConfigurationProperties() != null) {
-      factory.setConfigurationProperties(this.properties.getConfigurationProperties());
-    }
-    if (!ObjectUtils.isEmpty(this.interceptors)) {
-      factory.setPlugins(this.interceptors);
-    }
+    factory.setConfigurationProperties(this.properties.getConfigurationProperties());
+    factory.setTypeAliasesPackage(this.properties.getTypeAliasesPackage());
+    factory.setTypeAliases(this.properties.getTypeAliases());
+    factory.setTypeAliasesSuperType(this.properties.getTypeAliasesSuperType());
+    factory.setTypeHandlersPackage(this.properties.getTypeHandlersPackage());
+    factory.setTypeHandlers(this.properties.getTypeHandlers());
 
-    if (this.databaseIdProvider != null) {
-      factory.setDatabaseIdProvider(this.databaseIdProvider);
-    }
-    if (StringUtils.hasLength(this.properties.getTypeAliasesPackage())) {
-      factory.setTypeAliasesPackage(this.properties.getTypeAliasesPackage());
-    }
-    if (this.properties.getTypeAliasesSuperType() != null) {
-      factory.setTypeAliasesSuperType(this.properties.getTypeAliasesSuperType());
-    }
-    if (StringUtils.hasLength(this.properties.getTypeHandlersPackage())) {
-      factory.setTypeHandlersPackage(this.properties.getTypeHandlersPackage());
-    }
-    if (!ObjectUtils.isEmpty(this.typeHandlers)) {
-      factory.setTypeHandlers(this.typeHandlers);
-    }
-    if (!ObjectUtils.isEmpty(this.properties.resolveMapperLocations())) {
-      factory.setMapperLocations(this.properties.resolveMapperLocations());
+    Resource[] mapperLocations = JpaMybatisAutoConfiguration.resolveMapperLocations(
+        this.properties.getMapperLocations());
+    if (!ObjectUtils.isEmpty(mapperLocations)) {
+      factory.setMapperLocations(mapperLocations);
     }
 
     return factory.getObject();
@@ -115,7 +77,7 @@ public class JpaMybatisTemplateAutoConfiguration implements InitializingBean {
   @Bean("sqlSessionTemplate")
   @ConditionalOnMissingBean
   public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-      return new SqlSessionTemplate(sqlSessionFactory);
+    return new SqlSessionTemplate(sqlSessionFactory);
   }
 
 }
