a0b5821297011cdbc3fb4ee912c8adf9f7a496fb
Peter Wu <piterwu@outlook.com>
2022-03-31 16:02:08 +0800

    依赖配置

--- plugin/publish-plugin/build.gradle.kts
+++ plugin/publish-plugin/build.gradle.kts
@@ -4,7 +4,5 @@ dependencies {
     api("org.jetbrains.dokka:kotlin-as-java-plugin")
     api("org.jetbrains.dokka:dokka-gradle-plugin")
     api("com.gradle.publish:plugin-publish-plugin")
-    //    implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:0.30.0") {
-//        exclude("org.jetbrains.kotlin")
-//    }
+//    api("io.codearte.gradle.nexus:gradle-nexus-staging-plugin")
 }
