faa73dbd92dc939f25b1737a464d181c4b855947
Peter Wu <piterwu@outlook.com>
2022-04-15 12:49:10 +0800

    baseWeb 测试 异常信息处理调整

--- util/test/src/main/java/top/bettercode/summer/util/test/TestConfiguration.java
+++ util/test/src/main/java/top/bettercode/summer/util/test/TestConfiguration.java
@@ -6,21 +6,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.WebRequest;
 import top.bettercode.api.sign.ApiSignProperties;
-import top.bettercode.simpleframework.web.error.ErrorAttributes;
-import top.bettercode.simpleframework.web.error.MocTestErrorLoggingHandler;
 
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
 public class TestConfiguration {
-
-  @Bean
-  public MocTestErrorLoggingHandler mocTestErrorLoggingHandler(
-      ErrorAttributes errorAttributes,
-      @Autowired(required = false) WebRequest webRequest) {
-    return new MocTestErrorLoggingHandler(errorAttributes, webRequest);
-  }
 
   @ConditionalOnBean(ApiSignProperties.class)
   @Bean
