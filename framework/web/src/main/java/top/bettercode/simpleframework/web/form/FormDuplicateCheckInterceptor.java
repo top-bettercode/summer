73b20ca6637dbc864576c1bc1ed916d203e98de6
Peter Wu <piterwu@outlook.com>
2022-05-09 12:01:09 +0800

    防重复提交调整

--- framework/web/src/main/java/top/bettercode/simpleframework/web/form/FormDuplicateCheckInterceptor.java
+++ framework/web/src/main/java/top/bettercode/simpleframework/web/form/FormDuplicateCheckInterceptor.java
@@ -18,7 +18,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.method.HandlerMethod;
 import top.bettercode.lang.util.Sha512DigestUtils;
 import top.bettercode.lang.util.StringUtil;
-import top.bettercode.logging.RequestLoggingFilter;
 import top.bettercode.logging.trace.TraceHttpServletRequestWrapper;
 import top.bettercode.logging.trace.TraceServletInputStream;
 import top.bettercode.simpleframework.AnnotatedUtils;
@@ -54,46 +53,39 @@ public class FormDuplicateCheckInterceptor implements NotErrorHandlerInterceptor
           log.debug(request.getServletPath() + " formDuplicateCheck");
         }
         if (!hasFormKey) {
-          String username = (String) request.getAttribute(
-              RequestLoggingFilter.REQUEST_LOGGING_USERNAME);
-          if (StringUtils.hasText(username)) {
-            formkey = username;
-          } else {
-            ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(
-                request);
-            HttpHeaders httpHeaders = servletServerHttpRequest.getHeaders();
-            formkey = StringUtil.valueOf(httpHeaders);
+          ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(
+              request);
+          HttpHeaders httpHeaders = servletServerHttpRequest.getHeaders();
+          formkey = StringUtil.valueOf(httpHeaders);
 
-            if (isFormPost(request)) {
-              String params = StringUtil.valueOf(request.getParameterMap());
-              formkey += "::" + params;
-            } else {
-              TraceHttpServletRequestWrapper traceHttpServletRequestWrapper = getTraceHttpServletRequestWrapper(
-                  request);
-              if (traceHttpServletRequestWrapper != null) {
-                try {
-                  InputStream body = traceHttpServletRequestWrapper.getInputStream();
-                  if (body instanceof TraceServletInputStream) {
-                    formkey += "::" + StreamUtils.copyToString(body, Charset.defaultCharset());
-                    body.reset();
-                  } else {
-                    log.info(request.getServletPath()
-                        + " not traceServletInputStream ignore formDuplicateCheck");
-                    return true;
-                  }
-                } catch (IOException e) {
-                  log.info(
-                      request.getServletPath() + e.getMessage() + " ignore formDuplicateCheck");
+          if (isFormPost(request)) {
+            String params = StringUtil.valueOf(request.getParameterMap());
+            formkey += "::" + params;
+          } else {
+            TraceHttpServletRequestWrapper traceHttpServletRequestWrapper = getTraceHttpServletRequestWrapper(
+                request);
+            if (traceHttpServletRequestWrapper != null) {
+              try {
+                InputStream body = traceHttpServletRequestWrapper.getInputStream();
+                if (body instanceof TraceServletInputStream) {
+                  formkey += "::" + StreamUtils.copyToString(body, Charset.defaultCharset());
+                  body.reset();
+                } else {
+                  log.info(request.getServletPath()
+                      + " not traceServletInputStream ignore formDuplicateCheck");
                   return true;
                 }
-              } else {
-                log.info(request.getServletPath()
-                    + " not traceHttpServletRequestWrapper ignore formDuplicateCheck");
+              } catch (IOException e) {
+                log.info(
+                    request.getServletPath() + e.getMessage() + " ignore formDuplicateCheck");
                 return true;
               }
+            } else {
+              log.info(request.getServletPath()
+                  + " not traceHttpServletRequestWrapper ignore formDuplicateCheck");
+              return true;
             }
           }
-
         }
 
         String servletPath = request.getServletPath();
