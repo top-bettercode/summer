0fb149ac2e02448050c0d233f1f2be9d74aac285
Peter Wu <piterwu@outlook.com>
2022-05-09 19:35:21 +0800

    处理匿名用户权限

--- framework/security/src/main/java/top/bettercode/simpleframework/security/config/SecurityConfiguration.java
+++ framework/security/src/main/java/top/bettercode/simpleframework/security/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package top.bettercode.simpleframework.security.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -18,6 +19,7 @@ import org.springframework.core.Ordered;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +33,7 @@ import top.bettercode.simpleframework.config.CorsProperties;
 import top.bettercode.simpleframework.config.SummerWebProperties;
 import top.bettercode.simpleframework.security.ApiTokenEndpointFilter;
 import top.bettercode.simpleframework.security.ApiTokenService;
+import top.bettercode.simpleframework.security.DefaultAuthority;
 import top.bettercode.simpleframework.security.IResourceService;
 import top.bettercode.simpleframework.security.IRevokeTokenService;
 import top.bettercode.simpleframework.security.URLFilterInvocationSecurityMetadataSource;
@@ -141,9 +144,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
           Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
           //处理匿名用户权限
           if (configAttributes.size() == 1 && "ROLE_ANONYMOUS".equals(
-              configAttributes.iterator().next().getAttribute())
-              && resourceService.supportsAnonymous()) {
-            return;
+              configAttributes.iterator().next().getAttribute())) {
+            if (resourceService.supportsAnonymous()) {
+              return;
+            } else {
+              configAttributes = Collections.singletonList(
+                  new SecurityConfig(DefaultAuthority.DEFAULT_AUTHORITY_STRING));
+            }
           }
 
           if (log.isDebugEnabled()) {
