225e1ebfda32e9663792f3c294b28ce504951521
Peter Wu <piterwu@outlook.com>
2022-05-30 21:42:54 +0800

    异常优化

--- framework/web/src/main/java/top/bettercode/simpleframework/exception/BusinessException.java
+++ framework/web/src/main/java/top/bettercode/simpleframework/exception/BusinessException.java
@@ -2,64 +2,36 @@ package top.bettercode.simpleframework.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import top.bettercode.lang.property.PropertiesSource;
 
 /**
  * @author Peter Wu
  */
 @ResponseStatus(HttpStatus.OK)
-public class BusinessException extends RuntimeException {
+public class BusinessException extends SystemException {
 
-  private static final long serialVersionUID = -7941819415782111951L;
-  /**
-   * 业务错误码
-   */
-  private final String code;
-  private final Object data;
-  private static final PropertiesSource propertiesSource = PropertiesSource
-      .of("error-code", "properties.error-code");
+  private static final long serialVersionUID = 1L;
 
   public BusinessException(String code) {
-    super(propertiesSource.getOrDefault(code, code));
-    this.code = code;
-    this.data = null;
+    super(code);
   }
 
   public BusinessException(String code, Throwable cause) {
-    super(propertiesSource.getOrDefault(code, code), cause);
-    this.code = code;
-    this.data = null;
+    super(code, cause);
   }
 
   public BusinessException(String code, Object data) {
-    super(propertiesSource.getOrDefault(code, code));
-    this.code = code;
-    this.data = data;
+    super(code, data);
   }
 
   public BusinessException(String code, String message) {
-    super(message);
-    this.code = code;
-    this.data = null;
+    super(code, message);
   }
 
   public BusinessException(String code, String message, Throwable cause) {
-    super(message, cause);
-    this.code = code;
-    this.data = null;
+    super(code, message, cause);
   }
 
   public BusinessException(String code, String message, Object data) {
-    super(message);
-    this.code = code;
-    this.data = data;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public Object getData() {
-    return data;
+    super(code, message, data);
   }
 }
