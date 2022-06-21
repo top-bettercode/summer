3ccd16e7af1d00154f16b3fc8511320538f87046
Peter Wu <piterwu@outlook.com>
2022-06-21 15:15:33 +0800

    jenkins任务配置调整

--- plugin/project-plugin/src/main/kotlin/plugin/RootProjectTasks.kt
+++ plugin/project-plugin/src/main/kotlin/plugin/RootProjectTasks.kt
@@ -4,8 +4,6 @@ import org.gradle.api.Project
 import top.bettercode.generator.DatabaseDriver
 import top.bettercode.generator.GeneratorExtension
 import top.bettercode.generator.ddl.MysqlToDDL
-import top.bettercode.generator.ddl.OracleToDDL
-import top.bettercode.generator.ddl.SqlLiteToDDL
 import top.bettercode.generator.dom.unit.FileUnit
 import top.bettercode.gradle.generator.GeneratorPlugin
 
@@ -20,40 +18,59 @@ object RootProjectTasks {
         project.tasks.apply {
             val jenkinsJobs = project.findProperty("jenkins.jobs")?.toString()?.split(",")
                 ?.filter { it.isNotBlank() }
+            val jenkinsDevJobs = project.findProperty("jenkins.dev.jobs")?.toString()?.split(",")
+                ?.filter { it.isNotBlank() }
+            val jenkinsTestJobs = project.findProperty("jenkins.test.jobs")?.toString()?.split(",")
+                ?.filter { it.isNotBlank() }
+
+            val jobs = mutableMapOf<String, List<String>>()
+            if (!jenkinsJobs.isNullOrEmpty()) {
+                jobs["default"] = jenkinsJobs
+            }
+            if (!jenkinsDevJobs.isNullOrEmpty()) {
+                jobs["dev"] = jenkinsDevJobs
+            }
+            if (!jenkinsTestJobs.isNullOrEmpty()) {
+                jobs["test"] = jenkinsTestJobs
+            }
+
             val jenkinsServer = project.findProperty("jenkins.server")?.toString()
             val jenkinsAuth = project.findProperty("jenkins.auth")?.toString()
-            if (!jenkinsJobs.isNullOrEmpty() && !jenkinsAuth.isNullOrBlank() && !jenkinsServer.isNullOrBlank()) {
+            if (jobs.isNotEmpty() && !jenkinsAuth.isNullOrBlank() && !jenkinsServer.isNullOrBlank()) {
                 val jenkins = Jenkins(jenkinsServer, jenkinsAuth)
                 create("build[All]") {
                     it.group = "jenkins"
                     it.doLast {
-                        jenkinsJobs.forEach { jobName ->
+                        jobs.values.flatten().forEach { jobName ->
                             jenkins.build(jobName)
                         }
                     }
                 }
-                jenkinsJobs.forEach { jobName ->
-                    val jobTaskName = jobName.replace(
-                        "[()\\[\\]{}|/]|\\s*|\t|\r|\n|".toRegex(),
-                        ""
-                    )
-                    create("build[$jobTaskName]") {
-                        it.group = "jenkins"
-                        it.doLast {
-                            jenkins.build(jobName)
+                jobs.forEach { (env, jobNames) ->
+                    jobNames.forEach { jobName ->
+                        val jobTaskName = jobName.replace(
+                            "[()\\[\\]{}|/]|\\s*|\t|\r|\n|".toRegex(),
+                            ""
+                        )
+                        val envName = if (env == "default") "" else "[$env]"
+                        create("build$envName[$jobTaskName]") {
+                            it.group = "jenkins"
+                            it.doLast {
+                                jenkins.build(jobName)
+                            }
                         }
-                    }
-                    create("lastBuildInfo[$jobTaskName]") {
-                        it.group = "jenkins"
-                        it.doLast {
-                            jenkins.buildInfo(jobName)
+                        create("lastBuildInfo$envName[$jobTaskName]") {
+                            it.group = "jenkins"
+                            it.doLast {
+                                jenkins.buildInfo(jobName)
+                            }
                         }
-                    }
-                    create("description[$jobTaskName]") {
-                        it.group = "jenkins"
-                        it.doLast {
-                            val description = jenkins.description(jobName)
-                            println("job 描述信息：${if (description.isNotBlank()) "\n$description" else "无"}")
+                        create("description$envName[$jobTaskName]") {
+                            it.group = "jenkins"
+                            it.doLast {
+                                val description = jenkins.description(jobName)
+                                println("job 描述信息：${if (description.isNotBlank()) "\n$description" else "无"}")
+                            }
                         }
                     }
                 }
