da64e3f2d6269094a8546ca8209c377035796d45
Peter Wu <piterwu@outlook.com>
2022-06-23 13:50:12 +0800

    data jpa 动态更新 是否新实体 检查兼容未设置version

--- framework/data-jpa/src/test/java/top/bettercode/simpleframework/data/test/domain/User.java
+++ framework/data-jpa/src/test/java/top/bettercode/simpleframework/data/test/domain/User.java
@@ -1,7 +1,9 @@
 package top.bettercode.simpleframework.data.test.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.Version;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -10,6 +12,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class User extends BaseUser{
 
+  @Version
+  @Column(name = "version", columnDefinition = "INT(11) DEFAULT 0", length = 11)
+  private Integer version;
+
   public User() {
   }
 
@@ -17,4 +23,13 @@ public class User extends BaseUser{
     super(firstName, lastName);
   }
 
+
+  public Integer getVersion() {
+    return version;
+  }
+
+  public User setVersion(Integer version) {
+    this.version = version;
+    return this;
+  }
 }
