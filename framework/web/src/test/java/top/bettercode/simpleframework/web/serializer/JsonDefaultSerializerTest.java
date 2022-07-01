5e95b70cfc216f94eabfe7ec183bce5877c4e5b8
Peter Wu <piterwu@outlook.com>
2022-07-01 11:29:27 +0800

    默认值支持取其他字段

--- framework/web/src/test/java/top/bettercode/simpleframework/web/serializer/JsonDefaultSerializerTest.java
+++ framework/web/src/test/java/top/bettercode/simpleframework/web/serializer/JsonDefaultSerializerTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
 import top.bettercode.simpleframework.config.JacksonExtProperties;
 import top.bettercode.simpleframework.web.DataDicBean;
 import top.bettercode.simpleframework.web.serializer.annotation.JsonDefault;
@@ -16,23 +19,45 @@ import top.bettercode.simpleframework.web.serializer.annotation.JsonDefault;
 public class JsonDefaultSerializerTest {
 
   private final JacksonExtProperties jacksonExtProperties = new JacksonExtProperties();
+  ObjectMapper objectMapper = new ObjectMapper();
+
+  @BeforeEach
+  public void setUp() {
+    objectMapper.setDefaultPropertyInclusion(Include.NON_NULL);
+    objectMapper.setSerializerFactory(objectMapper.getSerializerFactory()
+        .withSerializerModifier(new CustomNullSerializerModifier(jacksonExtProperties)));
+
+    MockEnvironment mockEnvironment = new MockEnvironment();
+//    mockEnvironment.setProperty("summer.multipart.file-url-format", "/path%s");
+    mockEnvironment.setProperty("summer.multipart.file-url-format", "http://127.0.0.1%s");
+    mockEnvironment.setProperty("path1-url", "http://127.0.0.2%s");
+    UrlSerializer.setEnvironment(mockEnvironment);
+  }
 
   @Test
   public void serialize() throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.addMixIn(DataDicBean.class, DataDicBeanMin.class);
     objectMapper.setDefaultPropertyInclusion(Include.NON_NULL);
     objectMapper.setSerializerFactory(objectMapper.getSerializerFactory()
         .withSerializerModifier(new CustomNullSerializerModifier(jacksonExtProperties)));
 
     DataDicBean dicBean = new DataDicBean();
-    assertEquals("{\"price\":0,\"path\":\"/default.jpg\",\"path1\":\"\"}",
+    assertEquals(
+        "{\"number1\":0,\"number22\":null,\"price\":0,\"path\":\"/default.jpg\",\"pathUrl\":\"http://127.0.0.1/default.jpg\",\"path1\":\"\"}",
         objectMapper.writeValueAsString(dicBean));
-    dicBean.setPrice(100L);
+
+    dicBean.setNumber1(new BigDecimal(100));
     dicBean.setPath("/a.jpg");
     dicBean.setPath1("/b.jpg");
 
-    assertEquals("{\"price\":100,\"path\":\"/a.jpg\",\"path1\":\"/b.jpg\"}",
+    assertEquals(
+        "{\"number1\":100,\"number22\":null,\"price\":0,\"path\":\"/a.jpg\",\"pathUrl\":\"http://127.0.0.1/a.jpg\",\"path1\":\"/b.jpg\"}",
+        objectMapper.writeValueAsString(dicBean));
+
+    dicBean = new DataDicBean();
+    dicBean.setNumber2(new BigDecimal(10));
+    assertEquals(
+        "{\"number1\":0,\"number2\":10.000,\"number22\":10,\"price\":0,\"path\":\"/default.jpg\",\"pathUrl\":\"http://127.0.0.1/default.jpg\",\"path1\":\"\"}",
         objectMapper.writeValueAsString(dicBean));
 
   }
