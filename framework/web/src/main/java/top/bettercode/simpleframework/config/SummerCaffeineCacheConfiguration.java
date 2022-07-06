d4894f8da4f64b392c10e033a21696d0a649fd04
Peter Wu <piterwu@outlook.com>
2022-07-06 17:22:25 +0800

    缓存配置

--- framework/web/src/main/java/top/bettercode/simpleframework/config/SummerCaffeineCacheConfiguration.java
+++ framework/web/src/main/java/top/bettercode/simpleframework/config/SummerCaffeineCacheConfiguration.java
@@ -2,10 +2,8 @@ package top.bettercode.simpleframework.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.Map;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +14,21 @@ import org.springframework.context.annotation.Configuration;
  * @author Peter Wu
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnMissingBean(CacheManager.class)
-@AutoConfigureBefore(name = "org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration")
 @EnableConfigurationProperties(CaffeineCacheProperties.class)
 public class SummerCaffeineCacheConfiguration {
 
   @Bean
-  public CacheManager cacheManager(CaffeineCacheProperties caffeineCacheProperties) {
-    CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
-    Map<String, String> caches = caffeineCacheProperties.getCaches();
-    if (caches != null) {
-      caches.forEach((name, spec) -> caffeineCacheManager.registerCustomCache(name,
-          Caffeine.from(spec).build()));
-    } else {
-      caffeineCacheManager.setCacheSpecification(caffeineCacheProperties.getDefaultSpec());
-    }
-    return caffeineCacheManager;
+  public CacheManagerCustomizer<CaffeineCacheManager> caffeineCacheManagerCustomizer(
+      CaffeineCacheProperties caffeineCacheProperties) {
+    return cacheManager -> {
+      Map<String, String> caches = caffeineCacheProperties.getCaches();
+      if (caches != null) {
+        caches.forEach((name, spec) -> cacheManager.registerCustomCache(name,
+            Caffeine.from(spec).build()));
+      } else {
+        cacheManager.setCacheSpecification(caffeineCacheProperties.getDefaultSpec());
+      }
+    };
+
   }
 }
