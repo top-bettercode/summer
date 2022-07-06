0707d3678726c0d902d734b9ae9b64cb83284cda
Peter Wu <piterwu@outlook.com>
2022-07-06 22:59:02 +0800

    FormkeyService缓存配置

--- framework/web/src/main/java/top/bettercode/simpleframework/web/form/FormkeyService.java
+++ framework/web/src/main/java/top/bettercode/simpleframework/web/form/FormkeyService.java
@@ -5,6 +5,7 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
+import org.springframework.util.Assert;
 
 /**
  * 不支持 {@link FormDuplicateCheck} 自定义 expireSeconds
@@ -13,10 +14,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class FormkeyService implements IFormkeyService {
 
+  private final long expireSeconds;
   private final ConcurrentMap<String, Long> nameCache;
   private final ConcurrentMap<Long, ConcurrentMap<String, Boolean>> caches;
 
-  public FormkeyService(Long expireSeconds) {
+  public FormkeyService(long expireSeconds) {
+    Assert.isTrue(expireSeconds > 0, "过期时间必须大于0");
+    this.expireSeconds = expireSeconds;
     this.caches = new ConcurrentHashMap<>();
     Cache<String, Long> objectCache = Caffeine.newBuilder()
         .expireAfterWrite(expireSeconds, TimeUnit.SECONDS).build();
@@ -29,6 +33,7 @@ public class FormkeyService implements IFormkeyService {
 
   @Override
   public boolean exist(String formkey, long expireSeconds) {
+    expireSeconds = expireSeconds <= 0 ? this.expireSeconds : expireSeconds;
     Boolean present = getCache(expireSeconds).putIfAbsent(formkey, true);
     if (present == null) {
       nameCache.put(formkey, expireSeconds);
