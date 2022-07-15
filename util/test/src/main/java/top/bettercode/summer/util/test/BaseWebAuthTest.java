26a6ee5f472c1c2af8ca132e540ec22dc07dea47
Peter Wu <piterwu@outlook.com>
2022-07-15 15:19:27 +0800

    BaseWebAuthTest调整

--- util/test/src/main/java/top/bettercode/summer/util/test/BaseWebAuthTest.java
+++ util/test/src/main/java/top/bettercode/summer/util/test/BaseWebAuthTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import top.bettercode.simpleframework.security.ScopeUserDetailsService;
 import top.bettercode.simpleframework.security.UserDetailsAuthenticationToken;
 
 /**
@@ -14,13 +15,20 @@ import top.bettercode.simpleframework.security.UserDetailsAuthenticationToken;
 public abstract class BaseWebAuthTest extends BaseWebNoAuthTest {
 
   protected String username = "root";
+  protected String scope = null;
   @Autowired
   UserDetailsService userDetailsService;
 
   @Override
   public void defaultBeforeEach() throws Exception {
     beforeEach();
-    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    UserDetails userDetails;
+    if (scope != null && userDetailsService instanceof ScopeUserDetailsService) {
+      userDetails = ((ScopeUserDetailsService) userDetailsService).loadUserByScopeAndUsername(scope,
+          username);
+    } else {
+      userDetails = userDetailsService.loadUserByUsername(username);
+    }
     SecurityContextHolder.getContext()
         .setAuthentication(new UserDetailsAuthenticationToken(userDetails));
     System.err.println("------------------------------------------------------");
