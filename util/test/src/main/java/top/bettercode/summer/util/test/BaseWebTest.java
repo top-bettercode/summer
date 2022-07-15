b98b022baebed8fd7fb57e0e0eea529342bc7e07
Peter Wu <piterwu@outlook.com>
2022-07-15 14:33:02 +0800

    增加测试基础类

---
+++ util/test/src/main/java/top/bettercode/summer/util/test/BaseWebTest.java
@@ -0,0 +1,29 @@
+package top.bettercode.summer.util.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import top.bettercode.simpleframework.security.UserDetailsAuthenticationToken;
+
+/**
+ * mockMvc 基础测试类
+ *
+ * @author Peter Wu
+ */
+public abstract class BaseWebTest extends BaseWebNoAuthTest {
+
+  protected String username = "root";
+  @Autowired
+  UserDetailsService userDetailsService;
+
+  @Override
+  public void defaultBeforeEach() throws Exception {
+    beforeEach();
+    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+    SecurityContextHolder.getContext()
+        .setAuthentication(new UserDetailsAuthenticationToken(userDetails));
+  }
+
+
+}
