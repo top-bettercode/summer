269427d18f770ef7668497b91c7cf110b79016b8
Peter Wu <piterwu@outlook.com>
2022-11-20 11:06:13 +0800

    token解析优化

--- framework/security/src/main/java/top/bettercode/simpleframework/security/MultipleBearerTokenResolver.java
+++ framework/security/src/main/java/top/bettercode/simpleframework/security/MultipleBearerTokenResolver.java
@@ -3,19 +3,22 @@ package top.bettercode.simpleframework.security;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class MultipleBearerTokenResolver {
 
+  private final Logger log = LoggerFactory.getLogger(MultipleBearerTokenResolver.class);
   private static final Pattern authorizationPattern = Pattern
       .compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+=*)$",
           Pattern.CASE_INSENSITIVE);
 
-  private boolean allowFormEncodedBodyParameter = false;
+  private boolean allowFormEncodedBodyParameter = true;
 
-  private boolean allowUriQueryParameter = false;
+  private boolean allowUriQueryParameter = true;
 
   private String bearerTokenHeaderName = HttpHeaders.AUTHORIZATION;
   /**
@@ -25,16 +28,16 @@ public class MultipleBearerTokenResolver {
 
   public String resolve(HttpServletRequest request) {
     String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
-    if (authorizationHeaderToken == null) {
+    if (authorizationHeaderToken == null && isParameterTokenSupportedForRequest(request)) {
       authorizationHeaderToken = resolveFromRequestParameters(request,
           SecurityParameterNames.ACCESS_TOKEN);
     }
 
     if (compatibleAccessToken) {
       if (authorizationHeaderToken == null) {
-        authorizationHeaderToken = resolveFromHeader(request);
+        authorizationHeaderToken = resolveCompatibleAccessTokenFromHeader(request);
       }
-      if (authorizationHeaderToken == null) {
+      if (authorizationHeaderToken == null && isParameterTokenSupportedForRequest(request)) {
         authorizationHeaderToken = resolveFromRequestParameters(request,
             SecurityParameterNames.COMPATIBLE_ACCESS_TOKEN);
       }
@@ -53,8 +56,8 @@ public class MultipleBearerTokenResolver {
   }
 
   /**
-   * Set if transport of access token using URI query parameter is supported. Defaults to {@code
-   * false}.
+   * Set if transport of access token using URI query parameter is supported. Defaults to
+   * {@code false}.
    * <p>
    * The spec recommends against using this mechanism for sending bearer tokens, and even goes as
    * far as stating that it was only included for completeness.
@@ -69,8 +72,8 @@ public class MultipleBearerTokenResolver {
    * Set this value to configure what header is checked when resolving a Bearer Token. This value is
    * defaulted to {@link HttpHeaders#AUTHORIZATION}.
    * <p>
-   * This allows other headers to be used as the Bearer Token source such as {@link
-   * HttpHeaders#PROXY_AUTHORIZATION}
+   * This allows other headers to be used as the Bearer Token source such as
+   * {@link HttpHeaders#PROXY_AUTHORIZATION}
    *
    * @param bearerTokenHeaderName the header to check when retrieving the Bearer Token.
    * @since 5.4
@@ -89,11 +92,15 @@ public class MultipleBearerTokenResolver {
       return null;
     }
     Matcher matcher = authorizationPattern.matcher(authorization);
-    Assert.isTrue(matcher.matches(), "Bearer token is malformed");
-    return matcher.group("token");
+    if (matcher.matches()) {
+      return matcher.group("token");
+    } else {
+      log.warn("Bearer token is malformed");
+      return null;
+    }
   }
 
-  private String resolveFromHeader(HttpServletRequest request) {
+  private String resolveCompatibleAccessTokenFromHeader(HttpServletRequest request) {
     return request.getHeader(SecurityParameterNames.COMPATIBLE_ACCESS_TOKEN);
   }
 
@@ -106,8 +113,13 @@ public class MultipleBearerTokenResolver {
   }
 
   private boolean isParameterTokenSupportedForRequest(HttpServletRequest request) {
-    return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod()))
-        || (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
+    String method = request.getMethod();
+    return ((this.allowFormEncodedBodyParameter
+        && (RequestMethod.POST.name().equals(method)
+        || RequestMethod.PUT.name().equals(method)))
+        || (this.allowUriQueryParameter
+        && (RequestMethod.GET.name().equals(method)
+        || RequestMethod.DELETE.name().equals(method))));
   }
 
 }
