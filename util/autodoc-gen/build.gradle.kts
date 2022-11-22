5bc7253ca5048eed0bf7698294f1af1f48640efb
Peter Wu <piterwu@outlook.com>
2022-11-22 17:38:28 +0800

    配置

--- util/autodoc-gen/build.gradle.kts
+++ util/autodoc-gen/build.gradle.kts
@@ -17,7 +17,6 @@ dependencies {
     //test
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-jdbc")
     testImplementation("com.h2database:h2")
     testImplementation("xerces:xercesImpl")
 }
