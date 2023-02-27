d5f56a6d3a854fe27e8873e6ed59853ca91cfc82
Peter Wu <piterwu@outlook.com>
2023-02-27 14:42:11 +0800

    异常响应码配置

--- web/src/main/kotlin/top/bettercode/summer/web/support/gb2260/InvalidCodeException.kt
+++ web/src/main/kotlin/top/bettercode/summer/web/support/gb2260/InvalidCodeException.kt
@@ -1,5 +1,10 @@
 package top.bettercode.summer.web.support.gb2260
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 class InvalidCodeException(s: String?) : RuntimeException(s) {
     companion object {
         private const val serialVersionUID: Long = 1L
