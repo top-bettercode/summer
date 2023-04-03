47c44ff81ae25e35dd45587a0f04a8297cd17349
Peter Wu <piterwu@outlook.com>
2023-04-03 16:33:54 +0800

    管理授权配置调整

--- web/src/main/java/top/bettercode/summer/config/ManagementAuthProperties.java
+++ web/src/main/java/top/bettercode/summer/config/ManagementAuthProperties.java
@@ -1,8 +1,9 @@
 package top.bettercode.summer.config;
 
-import java.nio.charset.StandardCharsets;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * 日志访问权限 配置
@@ -12,51 +13,51 @@ import org.springframework.util.DigestUtils;
 @ConfigurationProperties("summer.management.auth")
 public class ManagementAuthProperties {
 
-  private String[] pattern = {};
-  /**
-   * 访问授权有效时间，单位：秒
-   */
-  private int maxAge = -1;
-  private String username = "madmin";
-  private String password;
+    private String[] pattern = {};
+    /**
+     * 访问授权有效时间，单位：秒
+     */
+    private int maxAge = -1;
+    private String username = "madmin";
+    private String password;
 
-  //--------------------------------------------
-  public String getAuthKey() {
-    return DigestUtils.md5DigestAsHex(
-        "${managementAuthProperties.username}:${managementAuthProperties.password}".getBytes(
-            StandardCharsets.UTF_8));
-  }
-  //--------------------------------------------
+    //--------------------------------------------
+    public String getAuthKey() {
+        return DigestUtils.md5DigestAsHex(
+                (username + ":" + password).getBytes(
+                        StandardCharsets.UTF_8));
+    }
+    //--------------------------------------------
 
-  public String[] getPattern() {
-    return pattern;
-  }
+    public String[] getPattern() {
+        return pattern;
+    }
 
-  public void setPattern(String[] pattern) {
-    this.pattern = pattern;
-  }
+    public void setPattern(String[] pattern) {
+        this.pattern = pattern;
+    }
 
-  public int getMaxAge() {
-    return maxAge;
-  }
+    public int getMaxAge() {
+        return maxAge;
+    }
 
-  public void setMaxAge(int maxAge) {
-    this.maxAge = maxAge;
-  }
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
 
-  public String getUsername() {
-    return username;
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
