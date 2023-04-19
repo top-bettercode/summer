c2c14765f83b4df8a2a67e6aac243912d7d458f0
Peter Wu <piterwu@outlook.com>
2023-04-19 18:00:43 +0800

    增加临时目录创建检查

--- web/src/main/java/top/bettercode/summer/web/config/MultipartFileConfiguration.java
+++ web/src/main/java/top/bettercode/summer/web/config/MultipartFileConfiguration.java
@@ -1,8 +1,8 @@
 package top.bettercode.summer.web.config;
 
-import java.io.File;
-import javax.servlet.MultipartConfigElement;
 import kotlin.collections.ArraysKt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
@@ -17,6 +17,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.bettercode.summer.web.resolver.multipart.MuipartFileToAttachmentConverter;
 
+import javax.servlet.MultipartConfigElement;
+import java.io.File;
+
 /**
  * 文件上传 配置
  *
@@ -24,36 +27,39 @@ import top.bettercode.summer.web.resolver.multipart.MuipartFileToAttachmentConve
  */
 @ConditionalOnProperty(prefix = "summer.multipart", value = "base-save-path")
 @EnableConfigurationProperties({MultipartProperties.class,
-    org.springframework.boot.autoconfigure.web.servlet.MultipartProperties.class})
+        org.springframework.boot.autoconfigure.web.servlet.MultipartProperties.class})
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
 @AutoConfigureBefore(MultipartAutoConfiguration.class)
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class MultipartFileConfiguration implements WebMvcConfigurer {
 
+    private final Logger log = LoggerFactory.getLogger(MultipartFileConfiguration.class);
 
-  public MultipartFileConfiguration(
-      MultipartProperties multipartProperties,
-      WebProperties webProperties) {
+    public MultipartFileConfiguration(
+            MultipartProperties multipartProperties,
+            WebProperties webProperties) {
 
-    Resources resources = webProperties.getResources();
-    resources.setStaticLocations(ArraysKt.plus(resources.getStaticLocations(),
-        multipartProperties.getStaticLocations()));
-  }
+        Resources resources = webProperties.getResources();
+        resources.setStaticLocations(ArraysKt.plus(resources.getStaticLocations(),
+                multipartProperties.getStaticLocations()));
+    }
 
-  @Bean
-  public MultipartConfigElement multipartConfigElement(
-      org.springframework.boot.autoconfigure.web.servlet.MultipartProperties multipartProperties,
-      MultipartProperties properties) {
-    File file = new File(properties.getBaseSavePath(), "tmp").getAbsoluteFile();
-    multipartProperties.setLocation(file.getAbsolutePath());
-    file.mkdirs();
-    return multipartProperties.createMultipartConfig();
-  }
+    @Bean
+    public MultipartConfigElement multipartConfigElement(
+            org.springframework.boot.autoconfigure.web.servlet.MultipartProperties multipartProperties,
+            MultipartProperties properties) {
+        File file = new File(properties.getBaseSavePath(), "tmp").getAbsoluteFile();
+        multipartProperties.setLocation(file.getAbsolutePath());
+        if (!file.exists() && !file.mkdirs()) {
+            log.error("创建临时目录失败：{}", file.getAbsolutePath());
+        }
+        return multipartProperties.createMultipartConfig();
+    }
 
-  @Bean
-  public MuipartFileToAttachmentConverter muipartFileToAttachmentConverter(
-      MultipartProperties multipartProperties) {
-    return new MuipartFileToAttachmentConverter(multipartProperties);
-  }
+    @Bean
+    public MuipartFileToAttachmentConverter muipartFileToAttachmentConverter(
+            MultipartProperties multipartProperties) {
+        return new MuipartFileToAttachmentConverter(multipartProperties);
+    }
 }
