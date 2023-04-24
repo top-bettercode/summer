bb9c7595871b306962c5a5d3a32eb83d0bbb30a2
Peter Wu <piterwu@outlook.com>
2023-04-24 12:25:05 +0800

    代码生成类型映射调整

--- tools/generator/src/main/kotlin/top/bettercode/summer/tools/generator/dom/java/JavaTypeResolver.kt
+++ tools/generator/src/main/kotlin/top/bettercode/summer/tools/generator/dom/java/JavaTypeResolver.kt
@@ -22,149 +22,149 @@ object JavaTypeResolver {
     init {
 
         typeMap[Types.ARRAY] = JdbcTypeInformation(
-            "ARRAY",
-            JavaType(java.lang.Object::class.java.name)
+                "ARRAY",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.BIGINT] = JdbcTypeInformation(
-            "BIGINT",
-            JavaType(java.lang.Long::class.java.name)
+                "BIGINT",
+                JavaType(java.lang.Long::class.java.name)
         )
         typeMap[Types.BINARY] = JdbcTypeInformation(
-            "BINARY",
-            JavaType("byte[]")
+                "BINARY",
+                JavaType("byte[]")
         )
         typeMap[Types.BIT] = JdbcTypeInformation(
-            "BIT",
-            JavaType(java.lang.Boolean::class.java.name)
+                "BIT",
+                JavaType(java.lang.Boolean::class.java.name)
         )
         typeMap[Types.BLOB] = JdbcTypeInformation(
-            "BLOB",
-            JavaType("byte[]")
+                "BLOB",
+                JavaType("byte[]")
         )
         typeMap[Types.BOOLEAN] = JdbcTypeInformation(
-            "BOOLEAN",
-            JavaType(java.lang.Boolean::class.java.name)
+                "BOOLEAN",
+                JavaType(java.lang.Boolean::class.java.name)
         )
         typeMap[Types.CHAR] = JdbcTypeInformation(
-            "CHAR",
-            JavaType(java.lang.String::class.java.name)
+                "CHAR",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.CLOB] = JdbcTypeInformation(
-            "CLOB",
-            JavaType(java.lang.String::class.java.name)
+                "CLOB",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.DATALINK] = JdbcTypeInformation(
-            "DATALINK",
-            JavaType(java.lang.Object::class.java.name)
+                "DATALINK",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.DATE] = JdbcTypeInformation(
-            "DATE",
-            JavaType(java.util.Date::class.java.name)
+                "DATE",
+                JavaType(java.util.Date::class.java.name)
         )
         typeMap[Types.DECIMAL] = JdbcTypeInformation(
-            "DECIMAL",
-            JavaType(java.math.BigDecimal::class.java.name)
+                "DECIMAL",
+                JavaType(java.math.BigDecimal::class.java.name)
         )
         typeMap[Types.DISTINCT] = JdbcTypeInformation(
-            "DISTINCT",
-            JavaType(java.lang.Object::class.java.name)
+                "DISTINCT",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.DOUBLE] = JdbcTypeInformation(
-            "DOUBLE",
-            JavaType(java.lang.Double::class.java.name)
+                "DOUBLE",
+                JavaType(java.lang.Double::class.java.name)
         )
         typeMap[Types.FLOAT] = JdbcTypeInformation(
-            "FLOAT",
-            JavaType(java.lang.Double::class.java.name)
+                "FLOAT",
+                JavaType(java.lang.Double::class.java.name)
         )
         typeMap[Types.INTEGER] = JdbcTypeInformation(
-            "INTEGER",
-            JavaType(java.lang.Integer::class.java.name)
+                "INTEGER",
+                JavaType(java.lang.Integer::class.java.name)
         )
         typeMap[Types.JAVA_OBJECT] = JdbcTypeInformation(
-            "JAVA_OBJECT",
-            JavaType(java.lang.Object::class.java.name)
+                "JAVA_OBJECT",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.LONGNVARCHAR] = JdbcTypeInformation(
-            "LONGNVARCHAR",
-            JavaType(java.lang.String::class.java.name)
+                "LONGNVARCHAR",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.LONGVARBINARY] = JdbcTypeInformation(
-            "LONGVARBINARY",
-            JavaType("byte[]")
+                "LONGVARBINARY",
+                JavaType("byte[]")
         )
         typeMap[Types.LONGVARCHAR] = JdbcTypeInformation(
-            "LONGVARCHAR",
-            JavaType(java.lang.String::class.java.name)
+                "LONGVARCHAR",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.NCHAR] = JdbcTypeInformation(
-            "NCHAR",
-            JavaType(java.lang.String::class.java.name)
+                "NCHAR",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.NCLOB] = JdbcTypeInformation(
-            "NCLOB",
-            JavaType(java.lang.String::class.java.name)
+                "NCLOB",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.NVARCHAR] = JdbcTypeInformation(
-            "NVARCHAR",
-            JavaType(java.lang.String::class.java.name)
+                "NVARCHAR",
+                JavaType(java.lang.String::class.java.name)
         )
         typeMap[Types.NULL] = JdbcTypeInformation(
-            "NULL",
-            JavaType(java.lang.Object::class.java.name)
+                "NULL",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.NUMERIC] = JdbcTypeInformation(
-            "NUMERIC",
-            JavaType(java.math.BigDecimal::class.java.name)
+                "NUMERIC",
+                JavaType(java.math.BigDecimal::class.java.name)
         )
         typeMap[Types.OTHER] = JdbcTypeInformation(
-            "OTHER",
-            JavaType(java.lang.Object::class.java.name)
+                "OTHER",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.REAL] = JdbcTypeInformation(
-            "REAL",
-            JavaType(java.lang.Float::class.java.name)
+                "REAL",
+                JavaType(java.lang.Float::class.java.name)
         )
         typeMap[Types.REF] = JdbcTypeInformation(
-            "REF",
-            JavaType(java.lang.Object::class.java.name)
+                "REF",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.SMALLINT] = JdbcTypeInformation(
-            "SMALLINT",
-            JavaType(java.lang.Short::class.java.name)
+                "SMALLINT",
+                JavaType(java.lang.Short::class.java.name)
         )
         typeMap[Types.STRUCT] = JdbcTypeInformation(
-            "STRUCT",
-            JavaType(java.lang.Object::class.java.name)
+                "STRUCT",
+                JavaType(java.lang.Object::class.java.name)
         )
         typeMap[Types.TIME] = JdbcTypeInformation(
-            "TIME",
-            JavaType(java.util.Date::class.java.name)
+                "TIME",
+                JavaType(java.util.Date::class.java.name)
         )
         typeMap[Types.TIMESTAMP] = JdbcTypeInformation(
-            "TIMESTAMP",
-            JavaType(java.util.Date::class.java.name)
+                "TIMESTAMP",
+                JavaType(java.util.Date::class.java.name)
         )
         typeMap[Types.TINYINT] = JdbcTypeInformation(
-            "TINYINT",
-            JavaType(java.lang.Byte::class.java.name)
+                "TINYINT",
+                JavaType(java.lang.Byte::class.java.name)
         )
         typeMap[Types.VARBINARY] = JdbcTypeInformation(
-            "VARBINARY",
-            JavaType("byte[]")
+                "VARBINARY",
+                JavaType("byte[]")
         )
         typeMap[Types.VARCHAR] = JdbcTypeInformation(
-            "VARCHAR",
-            JavaType(java.lang.String::class.java.name)
+                "VARCHAR",
+                JavaType(java.lang.String::class.java.name)
         )
         // JDK 1.8 types
         typeMap[Types.TIME_WITH_TIMEZONE] = JdbcTypeInformation(
-            "TIME_WITH_TIMEZONE",
-            JavaType("java.time.OffsetTime")
+                "TIME_WITH_TIMEZONE",
+                JavaType("java.time.OffsetTime")
         )
         typeMap[Types.TIMESTAMP_WITH_TIMEZONE] = JdbcTypeInformation(
-            "TIMESTAMP_WITH_TIMEZONE",
-            JavaType("java.time.OffsetDateTime")
+                "TIMESTAMP_WITH_TIMEZONE",
+                JavaType("java.time.OffsetDateTime")
         )
         typeMap.forEach { (t, u) ->
             typeNameMap[u.jdbcTypeName] = t
@@ -187,11 +187,13 @@ object JavaTypeResolver {
         answer = overrideDefaultType(column, answer)
 
         return if (forceIntegers && (JavaType(java.lang.Short::class.java.name) == answer || JavaType(
-                java.lang.Byte::class.java.name
-            ) == answer)
-        )
+                        java.lang.Byte::class.java.name
+                ) == answer)
+        ) {
             JavaType(java.lang.Integer::class.java.name)
-        else
+        } else if (Types.CHAR == column.dataType && column.columnSize == 1) {
+            JavaType(java.lang.Character::class.java.name)
+        } else
             answer
     }
 
@@ -202,7 +204,7 @@ object JavaTypeResolver {
 
     private fun calculateJdbcTypeInformation(column: Column): JdbcTypeInformation {
         return typeMap[calculateDataType(column)]
-            ?: throw IllegalStateException("无法计算${column.typeName}对应的Java类型，请在jdbcTypeName.properties中添加映射")
+                ?: throw IllegalStateException("无法计算${column.typeName}对应的Java类型，请在jdbcTypeName.properties中添加映射")
     }
 
     private fun overrideDefaultType(column: Column, defaultType: JavaType): JavaType {
@@ -211,7 +213,7 @@ object JavaTypeResolver {
         when (calculateDataType(column)) {
             Types.BIT -> answer = calculateBitReplacement(column, defaultType)
             Types.DECIMAL, Types.NUMERIC -> answer =
-                calculateBigDecimalReplacement(column, defaultType)
+                    calculateBigDecimalReplacement(column, defaultType)
 
             Types.DATE -> answer = calculateDateType(defaultType)
             Types.TIME -> answer = calculateTimeType(defaultType)
@@ -224,8 +226,8 @@ object JavaTypeResolver {
     }
 
     private fun calculateDataType(column: Column) =
-        calculateDataType(column.typeName) ?: column.dataType
-        ?: throw IllegalStateException("无法计算${column.typeName}对应的dataType类型，请在jdbcTypeName.properties中添加映射")
+            calculateDataType(column.typeName) ?: column.dataType
+            ?: throw IllegalStateException("无法计算${column.typeName}对应的dataType类型，请在jdbcTypeName.properties中添加映射")
 
     private fun calculateDateType(defaultType: JavaType): JavaType {
         return if (useJSR310Types) {
@@ -272,7 +274,7 @@ object JavaTypeResolver {
     }
 
     class JdbcTypeInformation(
-        val jdbcTypeName: String,
-        val javaType: JavaType
+            val jdbcTypeName: String,
+            val javaType: JavaType
     )
 }
