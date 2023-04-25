7116002c0a855b0065074b2d3c93a6add8029397
Peter Wu <piterwu@outlook.com>
2023-04-25 14:42:07 +0800

    BigDecimalSerializer 优化

--- web/src/main/java/top/bettercode/summer/web/serializer/annotation/JsonBigDecimal.java
+++ web/src/main/java/top/bettercode/summer/web/serializer/annotation/JsonBigDecimal.java
@@ -2,13 +2,10 @@ package top.bettercode.summer.web.serializer.annotation;
 
 import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.math.RoundingMode;
 import top.bettercode.summer.web.serializer.BigDecimalSerializer;
+
+import java.lang.annotation.*;
+import java.math.RoundingMode;
 
 /**
  * @author Peter Wu
@@ -21,25 +18,30 @@ import top.bettercode.summer.web.serializer.BigDecimalSerializer;
 @JsonSerialize(using = BigDecimalSerializer.class)
 public @interface JsonBigDecimal {
 
-  /**
-   * @return 小数位数
-   */
-  int scale() default 2;
+    /**
+     * @return 分数
+     */
+    String divisor() default "";
 
-  RoundingMode roundingMode() default RoundingMode.HALF_UP;
+    /**
+     * @return 小数位数
+     */
+    int scale() default 2;
 
-  /**
-   * @return 序列化为字符
-   */
-  boolean toPlainString() default false;
+    RoundingMode roundingMode() default RoundingMode.HALF_UP;
 
-  /**
-   * @return 当小数位为零时，是否精简小数位
-   */
-  boolean reduceFraction() default false;
+    /**
+     * @return 序列化为字符
+     */
+    boolean toPlainString() default false;
 
-  /**
-   * @return 扩展序列化百分比字段
-   */
-  boolean percent() default false;
+    /**
+     * @return 当小数位为零时，是否精简小数位
+     */
+    boolean reduceFraction() default false;
+
+    /**
+     * @return 扩展序列化百分比字段
+     */
+    boolean percent() default false;
 }
