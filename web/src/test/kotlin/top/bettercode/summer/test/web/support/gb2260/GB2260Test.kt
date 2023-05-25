d62565d121eb377c3accff57afea841a49678514
Peter Wu <piterwu@outlook.com>
2023-05-25 09:10:30 +0800

    测试补充行政区划经纬度

--- web/src/test/kotlin/top/bettercode/summer/test/web/support/gb2260/GB2260Test.kt
+++ web/src/test/kotlin/top/bettercode/summer/test/web/support/gb2260/GB2260Test.kt
@@ -3,14 +3,62 @@ package top.bettercode.summer.test.web.support.gb2260
 import org.junit.jupiter.api.Assertions.*
 
 import org.junit.jupiter.api.Test
+import org.springframework.web.client.RestTemplate
+import top.bettercode.summer.tools.lang.property.PropertiesSource
+import top.bettercode.summer.tools.lang.property.Settings
 import top.bettercode.summer.tools.lang.util.StringUtil
 import top.bettercode.summer.web.support.gb2260.GB2260
+import java.util.Properties
+import kotlin.io.path.toPath
 
 /**
  *
  * @author Peter Wu
  */
 class GB2260Test {
+
+    @Test
+    fun testReplenishLngLat() {
+        val propertiesSource = PropertiesSource.of("area")
+        val file = GB2260Test::class.java.getResource("/areaCode.properties").toURI().toPath().toFile()
+        //new Lines
+        val lines = mutableListOf<String>()
+        file.readLines().forEachIndexed { index, s ->
+            if (index == 0) {
+                lines.add(s)
+            } else {
+                val split = s.split("=")
+                val key = split[0]
+                val value = split[1]
+                val valext = propertiesSource[key]
+                lines.add("$key=${value.trim()}${if (valext != null) ",$valext" else ""}")
+            }
+        }
+        file.writeText(lines.joinToString("\n"))
+    }
+
+    @Test
+    fun testReplenishLngLat2(){
+        val template = RestTemplate()
+        val file = GB2260Test::class.java.getResource("/areaCode.properties").toURI().toPath().toFile()
+        //new Lines
+        val lines = mutableListOf<String>()
+        file.readLines().forEachIndexed { index, s ->
+            if (index == 0) {
+                lines.add(s)
+            } else {
+                val split = s.split("=")
+                val key = split[0]
+                val value = split[1]
+                //通过 RestTemplate 请求https://apis.map.qq.com/jsapi?qt=geoc&addr={0}&key=TU5BZ-MKD3W-L43RW-O3ZBW-GWMZK-QBB25&output=jsonp&pf=jsapi&ref=jsapi&cb=qq.maps._svcb1.geocoder0 获取经纬度
+                val entity = template.getForEntity("https://apis.map.qq.com/jsapi?qt=geoc&addr={0}&key=TU5BZ-MKD3W-L43RW-O3ZBW-GWMZK-QBB25&output=json&pf=jsapi&ref=jsapi&cb=qq.maps._svcb1.geocoder0", Map::class.java, key).body
+
+                val valext = "${entity["pointy"]},${entity["pointx"]}"
+                lines.add("$key=${value.trim()},$valext")
+            }
+        }
+        file.writeText(lines.joinToString("\n"))
+    }
 
     @Test
     fun getProvinces() {
@@ -27,26 +75,26 @@ class GB2260Test {
     @Test
     fun getDivisionByName() {
         System.err.println(
-            StringUtil.json(
-                GB2260.getDivisionByName(
-                    listOf(
-                        "河北省",
-                        "张家口市",
-                        "怀安县"
-                    )
-                ), true
-            )
+                StringUtil.json(
+                        GB2260.getDivisionByName(
+                                listOf(
+                                        "河北省",
+                                        "张家口市",
+                                        "怀安县"
+                                )
+                        ), true
+                )
         )
         System.err.println("=============================================================")
         System.err.println(
-            StringUtil.json(
-                GB2260.getDivisionByName(
-                    listOf(
-                        "河北省",
-                        "张家口市"
-                    )
-                ), true
-            )
+                StringUtil.json(
+                        GB2260.getDivisionByName(
+                                listOf(
+                                        "河北省",
+                                        "张家口市"
+                                )
+                        ), true
+                )
         )
     }
 
