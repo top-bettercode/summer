2434e9eff53d17c303bd89f6211b7dec04c1fcf9
Peter Wu <piterwu@outlook.com>
2023-07-10 15:04:00 +0800

    增加测试方法

--- test/src/main/kotlin/top/bettercode/summer/test/BaseTest.kt
+++ test/src/main/kotlin/top/bettercode/summer/test/BaseTest.kt
@@ -3,7 +3,9 @@ package top.bettercode.summer.test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
 import org.springframework.util.StringUtils
+import top.bettercode.summer.web.BaseController
 import top.bettercode.summer.web.support.ApplicationContextHolder.Companion.getProperty
+import java.util.function.Supplier
 
 /**
  * 基础测试类
@@ -16,4 +18,13 @@ abstract class BaseTest : BaseLogTest() {
     protected fun embeddedDatabase(): Boolean {
         return !StringUtils.hasText(getProperty("spring.datasource.url"))
     }
+
+    protected fun notFound(): Supplier<out RuntimeException?> {
+        return BaseController.notFound()
+    }
+
+    protected fun notFound(msg: String?): Supplier<out RuntimeException?> {
+        return BaseController.notFound(msg)
+    }
+
 }
