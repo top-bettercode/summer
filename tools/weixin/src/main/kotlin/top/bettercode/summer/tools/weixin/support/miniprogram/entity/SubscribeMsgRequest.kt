679a5fc69459204492ebeff8529bba6fb75a55f9
Peter Wu <piterwu@outlook.com>
2023-07-27 10:07:51 +0800

    推送参数调整

--- tools/weixin/src/main/kotlin/top/bettercode/summer/tools/weixin/support/miniprogram/entity/SubscribeMsgRequest.kt
+++ tools/weixin/src/main/kotlin/top/bettercode/summer/tools/weixin/support/miniprogram/entity/SubscribeMsgRequest.kt
@@ -7,27 +7,27 @@ import com.fasterxml.jackson.annotation.JsonProperty
  */
 data class SubscribeMsgRequest<T> @JvmOverloads constructor(
 
-    @field:JsonProperty("touser")
-    val touser: String? = null,
+        @field:JsonProperty("touser")
+        val touser: String,
 
-    @field:JsonProperty("template_id")
-    val templateId: String? = null,
+        @field:JsonProperty("template_id")
+        val templateId: String,
 
-    @field:JsonProperty("data")
-    val data: T,
+        @field:JsonProperty("data")
+        val data: T,
 
-    @field:JsonProperty("page")
-    val page: String? = null,
+        @field:JsonProperty("page")
+        val page: String? = null,
 
-    /**
-     * 跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
-     */
-    @field:JsonProperty("miniprogram_state")
-    var miniprogramState: String? = null,
+        /**
+         * 跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
+         */
+        @field:JsonProperty("miniprogram_state")
+        var miniprogramState: String? = "formal",
 
-    /**
-     * 进入小程序查看”的语言类型，支持zh_CN(简体中文)、en_US(英文)、zh_HK(繁体中文)、zh_TW(繁体中文)，默认为zh_CN
-     */
-    @field:JsonProperty("lang")
-    val lang: String? = "zh_CN"
+        /**
+         * 进入小程序查看”的语言类型，支持zh_CN(简体中文)、en_US(英文)、zh_HK(繁体中文)、zh_TW(繁体中文)，默认为zh_CN
+         */
+        @field:JsonProperty("lang")
+        val lang: String? = "zh_CN"
 )
