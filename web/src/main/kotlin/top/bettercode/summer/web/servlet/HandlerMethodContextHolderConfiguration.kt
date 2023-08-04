3c383701bef644815c09c692e1b41006d4cb7014
Peter Wu <piterwu@outlook.com>
2023-08-04 15:59:37 +0800

    no servlet 兼容

--- web/src/main/kotlin/top/bettercode/summer/web/servlet/HandlerMethodContextHolderConfiguration.kt
+++ web/src/main/kotlin/top/bettercode/summer/web/servlet/HandlerMethodContextHolderConfiguration.kt
@@ -1,9 +1,11 @@
 package top.bettercode.summer.web.servlet
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 
+@ConditionalOnBean(RequestMappingHandlerMapping::class)
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnWebApplication
 class HandlerMethodContextHolderConfiguration(
