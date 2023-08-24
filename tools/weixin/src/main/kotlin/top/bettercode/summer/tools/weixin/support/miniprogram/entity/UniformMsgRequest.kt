3c53acdb9fdec66c968060eb542104f4de28c419
Peter Wu <piterwu@outlook.com>
2023-08-24 14:27:56 +0800

    消息参数创建调整

--- tools/weixin/src/main/kotlin/top/bettercode/summer/tools/weixin/support/miniprogram/entity/UniformMsgRequest.kt
+++ tools/weixin/src/main/kotlin/top/bettercode/summer/tools/weixin/support/miniprogram/entity/UniformMsgRequest.kt
@@ -7,23 +7,23 @@ import com.fasterxml.jackson.annotation.JsonProperty
  *
  * 统一服务消息
  */
-data class UniformMsgRequest(
+data class UniformMsgRequest @JvmOverloads constructor(
 
         /**
          * 用户openid，可以是小程序的openid，也可以是mp_template_msg.appid对应的公众号的openid
          */
         @field:JsonProperty("touser")
         val touser: String,
+        /**
+         * 公众号模板消息相关的信息，可以参考公众号模板消息接口；有此节点并且没有weapp_template_msg节点时，发送公众号模板消息
+         */
+        @field:JsonProperty("mp_template_msg")
+        val mpTemplateMsg: MpTemplateMsg? = null,
 
         /**
          * 小程序模板消息相关的信息，可以参考小程序模板消息接口; 有此节点则优先发送小程序模板消息；（小程序模板消息已下线，不用传此节点）
          */
         @field:JsonProperty("weapp_template_msg")
-        val weappTemplateMsg: WeappTemplateMsg? = null,
+        val weappTemplateMsg: WeappTemplateMsg? = null
 
-        /**
-         * 公众号模板消息相关的信息，可以参考公众号模板消息接口；有此节点并且没有weapp_template_msg节点时，发送公众号模板消息
-         */
-        @field:JsonProperty("mp_template_msg")
-        val mpTemplateMsg: MpTemplateMsg? = null
 )
