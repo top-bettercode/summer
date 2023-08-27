242b6909010b308439f3a94635a11f9f053cdc88
Peter Wu <piterwu@outlook.com>
2023-08-27 21:34:55 +0800

    CachedValue 序列化

--- tools/weixin/src/main/kotlin/top/bettercode/summer/tools/weixin/support/offiaccount/entity/CachedValue.kt
+++ tools/weixin/src/main/kotlin/top/bettercode/summer/tools/weixin/support/offiaccount/entity/CachedValue.kt
@@ -1,12 +1,13 @@
 package top.bettercode.summer.tools.weixin.support.offiaccount.entity
 
+import java.io.Serializable
 import java.time.LocalDateTime
 
 /**
  *
  * @author Peter Wu
  */
-data class CachedValue(val value: String, val expiresIn: LocalDateTime) {
+data class CachedValue(val value: String, val expiresIn: LocalDateTime) : Serializable {
 
     val expired: Boolean by lazy { expiresIn <= LocalDateTime.now() }
 }
