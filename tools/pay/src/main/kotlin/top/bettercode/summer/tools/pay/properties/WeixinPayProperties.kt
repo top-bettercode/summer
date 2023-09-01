11deb489bc9aced61620d96af3b943268dd56629
Peter Wu <piterwu@outlook.com>
2023-09-01 10:48:45 +0800

    properties调整

--- tools/pay/src/main/kotlin/top/bettercode/summer/tools/pay/properties/WeixinPayProperties.kt
+++ tools/pay/src/main/kotlin/top/bettercode/summer/tools/pay/properties/WeixinPayProperties.kt
@@ -6,47 +6,47 @@ import org.springframework.boot.context.properties.ConfigurationProperties
  * 微售支付配置
  */
 @ConfigurationProperties(prefix = "summer.pay.weixin")
-class WeixinPayProperties {
-    /**
-     * API密钥
-     */
-    var apiKey: String? = null
+data class WeixinPayProperties(
+        /**
+         * 商户ID
+         */
+        var mchId: String? = null,
 
-    /**
-     * 商户ID
-     */
-    var mchId: String? = null
+        /**
+         * 应用ID
+         */
+        var appid: String? = null,
 
-    /**
-     * 应用ID
-     */
-    var appid: String? = null
+        /**
+         * API密钥
+         */
+        var apiKey: String? = null,
 
-    /**
-     * 通知地址
-     */
-    var notifyUrl: String? = null
-    /**
-     * 证书路径
-     */
-    var certPath: String? = null
-    /**
-     * 证书存储密码
-     */
-    var certStorePassword: String? = null
-    /**
-     * 证书Key密码
-     */
-    var certKeyPassword: String? = null
+        /**
+         * 通知地址
+         */
+        var notifyUrl: String? = null,
+        /**
+         * 证书路径
+         */
+        var certPath: String? = null,
+        /**
+         * 证书存储密码
+         */
+        var certStorePassword: String? = null,
+        /**
+         * 证书Key密码
+         */
+        var certKeyPassword: String? = null,
 
-    /**
-     * 请求连接超时时间毫秒数
-     */
-    var connectTimeout = 10000
+        /**
+         * 请求连接超时时间毫秒数
+         */
+        var connectTimeout: Int = 10000,
 
-    /**
-     * 请求读取超时时间毫秒数
-     */
-    var readTimeout = 10000
+        /**
+         * 请求读取超时时间毫秒数
+         */
+        var readTimeout: Int = 10000
 
-}
+)
