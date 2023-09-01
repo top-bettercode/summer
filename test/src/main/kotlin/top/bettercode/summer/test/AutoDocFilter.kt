3cc2836860522764eddb6961cdb52fa1b446a418
Peter Wu <piterwu@outlook.com>
2023-09-01 17:31:18 +0800

    测试contentType设置调整

--- test/src/main/kotlin/top/bettercode/summer/test/AutoDocFilter.kt
+++ test/src/main/kotlin/top/bettercode/summer/test/AutoDocFilter.kt
@@ -4,7 +4,9 @@ import org.springframework.core.Ordered
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
+import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.web.filter.OncePerRequestFilter
+import top.bettercode.summer.tools.lang.util.StringUtil
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -29,12 +31,27 @@ class AutoDocFilter(
             val servletRequest = AutoDocHttpServletRequest(request)
             handlers.filter { it.support(servletRequest) }.forEach { it.handle(servletRequest) }
             if (servletRequest.contentType.isNullOrBlank() && (HttpMethod.PUT.name == request.method || HttpMethod.POST.name == request.method)) {
-                servletRequest.extHeaders[HttpHeaders.CONTENT_TYPE] =
-                        arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                if (request is MockHttpServletRequest && request.contentLengthLong > 0L && isJson(request.contentAsByteArray!!)) {
+                    servletRequest.extHeaders[HttpHeaders.CONTENT_TYPE] =
+                            arrayOf(MediaType.APPLICATION_JSON_VALUE)
+                } else {
+                    servletRequest.extHeaders[HttpHeaders.CONTENT_TYPE] =
+                            arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+
+                }
             }
             filterChain.doFilter(servletRequest, response)
         } else {
             filterChain.doFilter(request, response)
+        }
+    }
+
+    fun isJson(content: ByteArray): Boolean {
+        return try {
+            StringUtil.objectMapper().readTree(content)
+            true
+        } catch (e: Exception) {
+            false
         }
     }
 }
