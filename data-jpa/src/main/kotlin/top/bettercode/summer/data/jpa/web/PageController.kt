afe0dbcd06400d634328c384faf198dc539b7490
Peter Wu <piterwu@outlook.com>
2023-10-31 12:30:32 +0800

    PageController调整

--- data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/web/PageController.kt
+++ data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/web/PageController.kt
@@ -17,25 +17,19 @@ import java.util.stream.Collectors
 open class PageController : BaseController() {
 
     @Autowired
-    private val properties: SpringDataWebProperties? = null
+    private lateinit var properties: SpringDataWebProperties
 
     override fun of(`object`: Any): RespExtra<*>? {
         return super.of(page(`object`))
     }
 
-    fun <T> ok(`object`: Page<T?>): ResponseEntity<*> {
-        return super.ok(pagedResources<T, Any>(`object`, null))
-    }
-
-    fun <T> ok(`object`: PageableList<T?>): ResponseEntity<*> {
-        return super.ok(pagedResources<T, Any>(`object`.toPage(), null))
-    }
-
-    protected fun <T, R> ok(`object`: Page<T?>, mapper: Function<T?, R?>): ResponseEntity<*> {
+    @JvmOverloads
+    protected fun <T, R> ok(`object`: Page<T?>, mapper: Function<T?, R?>? = null): ResponseEntity<*> {
         return super.ok(pagedResources(`object`, mapper))
     }
 
-    protected fun <T, R> ok(`object`: PageableList<T?>, mapper: Function<T?, R?>): ResponseEntity<*> {
+    @JvmOverloads
+    protected fun <T, R> ok(`object`: PageableList<T?>, mapper: Function<T?, R?>? = null): ResponseEntity<*> {
         return super.ok(pagedResources(`object`.toPage(), mapper))
     }
 
@@ -47,60 +41,42 @@ open class PageController : BaseController() {
         return super.ok(`object`.toList().stream().map(mapper).collect(Collectors.toList()))
     }
 
-    fun <T> page(`object`: Page<T?>): ResponseEntity<*> {
-        return super.ok(pagedResources<T, Any>(`object`, null))
-    }
-
-    fun <T> page(`object`: PageableList<T?>): ResponseEntity<*> {
-        return super.ok(pagedResources<T, Any>(`object`.toPage(), null))
-    }
-
-    fun <T> page(`object`: Collection<T?>): ResponseEntity<*> {
-        val number = if (properties!!.pageable.isOneIndexedParameters) 1 else 0
-        val size = `object`.size
-        val pagedResources = pagedResources(number.toLong(), size.toLong(), 1, size.toLong(), `object`)
-        return super.ok(pagedResources)
-    }
-
-    fun <T> page(`object`: Array<T?>): ResponseEntity<*> {
-        val number = if (properties!!.pageable.isOneIndexedParameters) 1 else 0
-        val size = `object`.size
-        val pagedResources = pagedResources(number.toLong(), size.toLong(), 1, size.toLong(), `object`.toList())
-
-        return super.ok(pagedResources)
-    }
-
     protected fun page(`object`: Any?): ResponseEntity<*> {
         return super.ok(`object`)
     }
 
-    protected fun <T, R> page(`object`: Array<T?>, mapper: Function<T?, R?>): ResponseEntity<*> {
-        val number = if (properties!!.pageable.isOneIndexedParameters) 1 else 0
-        val size = `object`.size
-        val collect = `object`.toList().stream().map(mapper).collect(Collectors.toList())
-        val pagedResources = pagedResources(number.toLong(), size.toLong(), 1, size.toLong(), collect)
-        return super.ok(pagedResources)
-    }
-
-    protected fun <T, R> page(`object`: Collection<T?>, mapper: Function<T?, R?>): ResponseEntity<*> {
-        val number = if (properties!!.pageable.isOneIndexedParameters) 1 else 0
-        val size = `object`.size
-        val collect = `object`.stream().map(mapper).collect(Collectors.toList())
-        val pagedResources = pagedResources(number.toLong(), size.toLong(), 1, size.toLong(), collect)
-        return super.ok(pagedResources)
-    }
-
-    protected fun <T, R> page(`object`: PageableList<T?>, mapper: Function<T?, R?>): ResponseEntity<*> {
-        return super.ok(pagedResources(`object`.toPage(), mapper))
-    }
-
-    protected fun <T, R> page(`object`: Page<T?>, mapper: Function<T?, R?>): ResponseEntity<*> {
+    @JvmOverloads
+    protected fun <T, R> page(`object`: Page<T?>, mapper: Function<T?, R?>? = null): ResponseEntity<*> {
         return super.ok(pagedResources(`object`, mapper))
     }
 
     @JvmOverloads
+    protected fun <T, R> page(`object`: PageableList<T?>, mapper: Function<T?, R?>? = null): ResponseEntity<*> {
+        return super.ok(pagedResources(`object`.toPage(), mapper))
+    }
+
+    @JvmOverloads
+    protected fun <T, R> page(`object`: Collection<T?>, mapper: Function<T?, R?>? = null): ResponseEntity<*> {
+        val number = if (properties.pageable.isOneIndexedParameters) 1 else 0
+        val size = `object`.size
+        val collect = if (mapper == null) `object` else `object`.stream().map(mapper).collect(Collectors.toList())
+        val pagedResources = pagedResources(number.toLong(), size.toLong(), 1, size.toLong(), collect)
+        return super.ok(pagedResources)
+    }
+
+    @JvmOverloads
+    protected fun <T, R> page(`object`: Array<T?>, mapper: Function<T?, R?>? = null): ResponseEntity<*> {
+        val number = if (properties.pageable.isOneIndexedParameters) 1 else 0
+        val size = `object`.size
+        val collect = if (mapper == null) `object`.toList() else `object`.toList().stream().map(mapper).collect(Collectors.toList())
+        val pagedResources = pagedResources(number.toLong(), size.toLong(), 1, size.toLong(), collect)
+        return super.ok(pagedResources)
+    }
+
+
+    @JvmOverloads
     protected fun <T, R> pagedResources(`object`: Page<T?>, mapper: Function<T?, R?>? = null): Any {
-        val number = if (properties!!.pageable.isOneIndexedParameters) `object`.number + 1 else `object`.number
+        val number = if (properties.pageable.isOneIndexedParameters) `object`.number + 1 else `object`.number
         val content = if (mapper == null) `object`.content else `object`.content.stream().map(mapper).collect(Collectors.toList())
         return pagedResources(number.toLong(), `object`.size.toLong(), `object`.totalPages.toLong(), `object`.totalElements, content)
     }
