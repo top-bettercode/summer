a200b15bb7d82317c0a2e38cafaa46047c00c486
Peter Wu <piterwu@outlook.com>
2023-11-03 17:07:33 +0800

    类型转换

--- data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/support/JpaUtil.kt
+++ data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/support/JpaUtil.kt
@@ -1,23 +1,16 @@
-@file:Suppress("DEPRECATION")
-
 package top.bettercode.summer.data.jpa.support
 
-import org.hibernate.type.descriptor.java.JavaTypeDescriptor
-import org.hibernate.type.descriptor.java.JavaTypeDescriptorRegistry
-import org.springframework.util.ClassUtils
+import top.bettercode.summer.web.support.ApplicationContextHolder
 
 /**
  * @author Peter Wu
  */
 object JpaUtil {
-    private val TYPE_DESCRIPTOR_REGISTRY = JavaTypeDescriptorRegistry.INSTANCE
 
     @Suppress("UNCHECKED_CAST")
     fun <T> convert(source: Any?, targetType: Class<T>?): T? {
         return if (source != null && !targetType!!.isInstance(source)) {
-            val descriptor = TYPE_DESCRIPTOR_REGISTRY.getDescriptor(
-                    ClassUtils.resolvePrimitiveIfNecessary(targetType)) as JavaTypeDescriptor<T>
-            descriptor.wrap(source, null)
+            ApplicationContextHolder.conversionService.convert(source, targetType)
         } else {
             source as T?
         }
