57e7c4b83d1f8e610d937b24ca61bd776b28100f
Peter Wu <piterwu@outlook.com>
2023-11-06 11:33:51 +0800

    命名调整

--- data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/web/MybatisPageController.kt
+++ data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/web/MybatisPageController.kt
@@ -3,10 +3,10 @@ package top.bettercode.summer.data.jpa.web
 /**
  * @author Peter Wu
  */
-open class CompatiblePageController : PageController() {
+open class MybatisPageController : PageController() {
 
     override fun <T> pagedResources(number: Long, size: Long, totalPages: Long, totalElements: Long, content: Collection<T?>): Any {
-        return CompatiblePagedResources(number, size, totalPages, totalElements, content)
+        return MybatisPagedResources(number, size, totalPages, totalElements, content)
     }
 
 }
--- data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/web/MybatisPagedResources.kt
+++ data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/web/MybatisPagedResources.kt
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page
 /**
  * @author Peter Wu
  */
-class CompatiblePagedResources<T>(
+class MybatisPagedResources<T>(
         /**
          * 当前页
          */
