e216d5b33b43ab7d36c8ae6e6670e84d78550a5f
Peter Wu <piterwu@outlook.com>
2023-11-13 16:53:28 +0800

    微信接口请求调整

--- tools/pay/src/main/kotlin/top/bettercode/summer/tools/pay/weixin/entity/SignMap.kt
+++ tools/pay/src/main/kotlin/top/bettercode/summer/tools/pay/weixin/entity/SignMap.kt
@@ -1,9 +1,12 @@
 package top.bettercode.summer.tools.pay.weixin.entity
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
 /**
  *
  * @author Peter Wu
  */
+@JacksonXmlRootElement(localName = "xml")
 class SignMap : HashMap<String, Any?>() {
     var sign: String?
         get() = this["sign"] as String?
