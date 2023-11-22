2de19eea759e90c1ae187f515479eaaf57da1182
Peter Wu <piterwu@outlook.com>
2023-11-22 11:31:37 +0800

    注释

--- tools/excel/src/main/kotlin/top/bettercode/summer/tools/excel/ExcelField.kt
+++ tools/excel/src/main/kotlin/top/bettercode/summer/tools/excel/ExcelField.kt
@@ -383,7 +383,7 @@ class ExcelField<T, P : Any?> {
 
     /**
      * @param format 格式 [说明...](https://learn.microsoft.com/en-us/dotnet/api/documentformat.openxml.spreadsheet.numberingformat?view=openxml-2.8.1)
-     * 示例：yyyy"年"m"月"d"日" hh"时"mm"分"ss"秒"
+     * 示例：yyyy年m月d日 hh时mm分ss秒
      * @return this
      */
     @JvmOverloads
@@ -565,7 +565,7 @@ class ExcelField<T, P : Any?> {
 
     /**
      * @param format 格式 [说明...](https://learn.microsoft.com/en-us/dotnet/api/documentformat.openxml.spreadsheet.numberingformat?view=openxml-2.8.1)
-     * 示例：yyyy"年"m"月"d"日" hh"时"mm"分"ss"秒"
+     * 示例：yyyy年m月d日 hh时mm分ss秒
      * @return this
      */
     fun format(format: String?): ExcelField<T, P> {
