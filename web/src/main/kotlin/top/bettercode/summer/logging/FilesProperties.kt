a895bcdd9314a83e01b64ad29761936ff35ba8bd
Peter Wu <piterwu@outlook.com>
2023-12-07 21:22:29 +0800

    注释

--- web/src/main/kotlin/top/bettercode/summer/logging/FilesProperties.kt
+++ web/src/main/kotlin/top/bettercode/summer/logging/FilesProperties.kt
@@ -3,20 +3,33 @@ package top.bettercode.summer.logging
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 /**
- * logging file 配置
+ * 配置 logging file
  *
  * @author Peter Wu
  */
 @Suppress("ConfigurationProperties")
 @ConfigurationProperties("summer.logging.files")
 open class FilesProperties {
+    /**
+     * 查看路径
+     */
     var viewPath: String? = null
+    /**
+     * 路径
+     */
     var path: String? = null
+    /**
+     * 最大文件大小
+     */
     var maxFileSize = "10MB"
+
+    /**
+     * 最大历史记录数量
+     */
     var maxHistory = 0
 
     /**
-     * 是否启动时滚动日志
+     * 启动时是否滚动日志
      */
     var isRolloverOnStart = true
 }
