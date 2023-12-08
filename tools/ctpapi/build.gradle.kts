ed11f0de9037a66166b630320ace4dc5d65df4fe
Peter Wu <piterwu@outlook.com>
2023-12-08 14:17:51 +0800

    版本号调整

--- tools/ctpapi/build.gradle.kts
+++ tools/ctpapi/build.gradle.kts
@@ -2,14 +2,16 @@ plugins {
     `java-library`
 }
 
-//version = "6.3.13_20181119"
-//version = "6.3.15_20190220"
-//version = "v6.6.1_P1_20210406"
-version = "v6.6.1_P1_CP_20210406"
+//val ctpVersion = "6.3.13_20181119"
+//val ctpVersion = "6.3.15_20190220"
+//val ctpVersion = "v6.6.1_P1_20210406"
+val ctpVersion = "v6.6.1_P1_CP_20210406"
+
+version = "$ctpVersion-SNAPSHOT"
 
 dependencies {
     implementation(project(":tools:tools"))
-    implementation(fileTree("libs/$version"))
+    implementation(fileTree("libs/$ctpVersion"))
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
@@ -17,12 +19,12 @@ dependencies {
 tasks {
 
     "jar"(Jar::class) {
-        from(fileTree(mapOf("dir" to "libs/${project.version}")).files.map { zipTree(it) })
+        from(fileTree(mapOf("dir" to "libs/$ctpVersion")).files.map { zipTree(it) })
     }
 
     @Suppress("UnstableApiUsage")
     "processResources"(ProcessResources::class) {
-        from(file("native/$version")) {
+        from(file("native/$ctpVersion")) {
             into("native")
         }
     }
