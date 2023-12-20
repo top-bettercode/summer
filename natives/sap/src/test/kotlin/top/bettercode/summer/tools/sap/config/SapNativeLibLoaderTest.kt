07698fb19bd1bebe33ad1049491cbc9827268485
Peter Wu <piterwu@outlook.com>
2023-12-20 12:31:40 +0800

    增加sap测试

---
+++ natives/sap/src/test/kotlin/top/bettercode/summer/tools/sap/config/SapNativeLibLoaderTest.kt
@@ -0,0 +1,15 @@
+package top.bettercode.summer.tools.sap.config
+
+import org.junit.jupiter.api.Test
+
+/**
+ *
+ * @author Peter Wu
+ */
+class SapNativeLibLoaderTest {
+
+    @Test
+    fun loadNativeLib() {
+        SapNativeLibLoader.loadNativeLib()
+    }
+}
