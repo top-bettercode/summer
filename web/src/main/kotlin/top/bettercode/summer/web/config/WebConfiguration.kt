6132020f5093100aac3de9c26c9d58e8ad73aa83
Peter Wu <piterwu@outlook.com>
2023-12-27 11:33:21 +0800

    配置

--- web/src/main/kotlin/top/bettercode/summer/web/config/WebConfiguration.kt
+++ web/src/main/kotlin/top/bettercode/summer/web/config/WebConfiguration.kt
@@ -2,6 +2,7 @@ package top.bettercode.summer.web.config
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext
@@ -60,6 +61,7 @@ class WebConfiguration {
         }
     }
 
+    @ConditionalOnClass(javax.servlet.http.HttpServletRequest::class)
     @ConditionalOnMissingBean(IFormkeyService::class)
     @Bean
     fun formkeyService(summerWebProperties: SummerWebProperties): IFormkeyService {
