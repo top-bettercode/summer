cd0a6cf94ea245962b5f602d0f8c9085aa0b266c
Peter Wu <piterwu@outlook.com>
2024-01-02 17:00:08 +0800

    日志调整

--- tools/tools/src/main/kotlin/top/bettercode/summer/tools/lang/PrettyMessageHTMLLayout.kt
+++ tools/tools/src/main/kotlin/top/bettercode/summer/tools/lang/PrettyMessageHTMLLayout.kt
@@ -19,7 +19,7 @@ class PrettyMessageHTMLLayout : HTMLLayout() {
     companion object {
         fun anchor(msg: String): String {
             val s = msg.trim().substringBefore(StringUtil.LINE_SEPARATOR)
-            val regex = Regex(".*(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{3} +[A-Z]+) .*")
+            val regex = Regex(".*(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.\\d{3} +[A-Z]+) (?s).*")
             return if (s.matches(regex)) {
                 s.replace(regex, "$1").split(' ').filter { it.isNotBlank() }.joinToString("-")
                         .lowercase(Locale.getDefault())
