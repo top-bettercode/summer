f05b8ef3c5ca2724ec00e02914ff55fedafc9c02
Peter Wu <piterwu@outlook.com>
2024-02-27 09:50:33 +0800

    配方测试

--- tools/recipe/src/test/kotlin/top/bettercode/summer/tools/recipe/RecipeSolverTest.kt
+++ tools/recipe/src/test/kotlin/top/bettercode/summer/tools/recipe/RecipeSolverTest.kt
@@ -25,10 +25,10 @@ import java.io.File
 internal class RecipeSolverTest {
     val products = listOf(
             "13-05-07高氯枸磷",
-//            "24-06-10高氯枸磷",
-//            "15-15-15喷浆硫基",
-//            "15-15-15喷浆氯基",
-//            "15-15-15常规氯基"
+            "24-06-10高氯枸磷",
+            "15-15-15喷浆硫基",
+            "15-15-15喷浆氯基",
+            "15-15-15常规氯基"
     )
     val solveTypes = listOf(
             SolverType.COPT,
@@ -43,7 +43,7 @@ internal class RecipeSolverTest {
     val nutrientUnchanged = true
     val materialUnchanged = true
     val solveTimes = 1
-    val toExcel = true
+    val toExcel = false
     val epsilon = 1e-4
 
     @Test
