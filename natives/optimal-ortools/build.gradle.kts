6b55e7f2cc563fd142ffa901f60cf3f997e92562
Peter Wu <piterwu@outlook.com>
2024-03-07 14:37:19 +0800

    测试日志依赖

--- natives/optimal-ortools/build.gradle.kts
+++ natives/optimal-ortools/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     api(project(":natives:optimal"))
     api("com.google.ortools:ortools-java")
 
+    testImplementation("org.slf4j:slf4j-simple")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 }
 
