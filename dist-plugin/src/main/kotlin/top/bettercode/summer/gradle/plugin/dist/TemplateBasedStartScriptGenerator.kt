a699047c19c289e5b8d9e01119558a3072b11fb1
Peter Wu <piterwu@outlook.com>
2024-05-08 13:35:14 +0800

    dist 脚本调整

--- dist-plugin/src/main/kotlin/top/bettercode/summer/gradle/plugin/dist/TemplateBasedStartScriptGenerator.kt
+++ dist-plugin/src/main/kotlin/top/bettercode/summer/gradle/plugin/dist/TemplateBasedStartScriptGenerator.kt
@@ -17,16 +17,16 @@ import org.gradle.util.internal.TextUtil
 import java.io.*
 
 class TemplateBasedStartScriptGenerator(
-        private val project: Project,
-        private val dist: DistExtension,
-        private val windows: Boolean,
-        private val includeJdk: Boolean,
-        private val includeNative: Boolean
+    private val project: Project,
+    private val dist: DistExtension,
+    private val windows: Boolean,
+    private val includeJdk: Boolean,
+    private val includeNative: Boolean
 ) : TemplateBasedScriptGenerator {
     private val lineSeparator: String =
-            if (windows) TextUtil.getWindowsLineSeparator() else TextUtil.getUnixLineSeparator()
+        if (windows) TextUtil.getWindowsLineSeparator() else TextUtil.getUnixLineSeparator()
     private val bindingFactory: StartScriptTemplateBindingFactory =
-            if (windows) StartScriptTemplateBindingFactory.windows() else StartScriptTemplateBindingFactory.unix()
+        if (windows) StartScriptTemplateBindingFactory.windows() else StartScriptTemplateBindingFactory.unix()
     private var template: TextResource
 
     init {
@@ -59,21 +59,21 @@ class TemplateBasedStartScriptGenerator(
             }
 
             val newDetails =
-                    DefaultJavaAppStartScriptGenerationDetails(
-                            details.applicationName,
-                            details.optsEnvironmentVar,
-                            details.exitEnvironmentVar,
-                            details.mainClassName,
-                            defaultJvmOpts,
-                            classpath,
-                            details.modulePath,
-                            details.scriptRelPath,
-                            details.appNameSystemProperty
-                    )
+                DefaultJavaAppStartScriptGenerationDetails(
+                    details.applicationName,
+                    details.optsEnvironmentVar,
+                    details.exitEnvironmentVar,
+                    details.mainClassName,
+                    defaultJvmOpts,
+                    classpath,
+                    details.modulePath,
+                    details.scriptRelPath,
+                    details.appNameSystemProperty
+                )
             val binding = bindingFactory.transform(newDetails)
             val jvmOpts = binding["defaultJvmOpts"]
             binding["defaultJvmOpts"] =
-                    if (windows) jvmOpts?.replace("%%", "%") else jvmOpts?.replace("\\$", "$")
+                if (windows) jvmOpts?.replace("%%", "%") else jvmOpts?.replace("\\$", "$")
             val scriptContent = generateStartScriptContentFromTemplate(binding)
             destination.write(scriptContent)
         } catch (e: IOException) {
@@ -105,47 +105,57 @@ class TemplateBasedStartScriptGenerator(
     private fun template(): StringBackedTextResource {
         val filename = if (windows) "windowsStartScript.txt" else "unixStartScript.txt"
         val stream: InputStream =
-                UnixStartScriptGenerator::class.java.getResourceAsStream(
-                        filename
-                )
-                        ?: throw IllegalStateException("Could not find class path resource " + filename + " relative to " + UnixStartScriptGenerator::class.java.name)
+            UnixStartScriptGenerator::class.java.getResourceAsStream(
+                filename
+            )
+                ?: throw IllegalStateException("Could not find class path resource " + filename + " relative to " + UnixStartScriptGenerator::class.java.name)
         val bufferedReader = BufferedReader(
-                InputStreamReader(stream, Charsets.UTF_8)
+            InputStreamReader(stream, Charsets.UTF_8)
         )
         var text = bufferedReader.readText()
         if (includeJdk) {
             text = if (windows) {
                 val location = "@rem Add default JVM options here"
                 StringBuilder(text).insert(
-                        text.indexOf(location),
-                        "@rem Set JAVA_HOME.\r\nset JAVA_HOME=%APP_HOME%\\\\jdk\r\n\r\n"
+                    text.indexOf(location),
+                    "@rem Set JAVA_HOME.\r\nset JAVA_HOME=%APP_HOME%\\\\jdk\r\n\r\n"
                 ).toString()
             } else {
                 val location = "# Add default JVM options here"
                 StringBuilder(text).insert(
-                        text.indexOf(location),
-                        "# Set JAVA_HOME.\nJAVA_HOME=\"\\\$APP_HOME/jdk\"\n\n"
+                    text.indexOf(location),
+                    "# Set JAVA_HOME.\nJAVA_HOME=\"\\\$APP_HOME/jdk\"\n\n"
                 ).toString()
             }
         }
-        if (!windows) {
+
+        if (windows) {
+            text = StringBuilder(text).insert(
+                text.indexOf("@rem Add default JVM options here") - 1,
+                "\r\nmkdir \"%APP_HOME%\\\\build\\\\native\" /p\r\n"
+            ).toString()
+        } else {
+            text = StringBuilder(text).insert(
+                text.indexOf("# Add default JVM options here") - 1,
+                "\nmkdir -p \\\$APP_HOME/build/native\n"
+            ).toString()
 
             text = StringBuilder(text).insert(
-                    text.indexOf("# Use the maximum available") - 1,
-                    "DEFAULT_JVM_OPTS=`eval echo \\\$DEFAULT_JVM_OPTS`\n"
+                text.indexOf("# Use the maximum available") - 1,
+                "DEFAULT_JVM_OPTS=`eval echo \\\$DEFAULT_JVM_OPTS`\n"
             ).toString()
 
             if (includeNative) {
                 text = StringBuilder(text).insert(
-                        text.indexOf("# Use the maximum available") - 1,
-                        "\nexport LD_LIBRARY_PATH=\\\$LD_LIBRARY_PATH:${dist.linuxLDLibraryPath(project)}\n"
+                    text.indexOf("# Use the maximum available") - 1,
+                    "\nexport LD_LIBRARY_PATH=\\\$LD_LIBRARY_PATH:${dist.linuxLDLibraryPath(project)}\n"
                 ).toString()
             }
         }
 
         return StringBackedTextResource(
-                GradleUserHomeTemporaryFileProvider { project.gradle.gradleUserHomeDir },
-                text
+            GradleUserHomeTemporaryFileProvider { project.gradle.gradleUserHomeDir },
+            text
         )
     }
 
