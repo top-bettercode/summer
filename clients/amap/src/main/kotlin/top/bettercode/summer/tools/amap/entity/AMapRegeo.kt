d13e7ebd8f32fb7d258d5c2fa788f2ed2c0e8fba
Peter Wu <piterwu@outlook.com>
2024-05-27 16:40:38 +0800

    地图接口适配

--- clients/amap/src/main/kotlin/top/bettercode/summer/tools/amap/entity/AMapRegeo.kt
+++ clients/amap/src/main/kotlin/top/bettercode/summer/tools/amap/entity/AMapRegeo.kt
@@ -1,9 +1,11 @@
 package top.bettercode.summer.tools.amap.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import top.bettercode.summer.tools.lang.client.ClientResponse
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class AMapRegeo(
     @field:JsonProperty("status")
     val status: String? = null,
--- clients/amap/src/main/kotlin/top/bettercode/summer/tools/amap/entity/StreetNumber.kt
+++ clients/amap/src/main/kotlin/top/bettercode/summer/tools/amap/entity/StreetNumber.kt
@@ -5,17 +5,54 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class StreetNumber(
 
         @field:JsonProperty("number")
-        val number: String? = null,
+        val number: Array<String>? = null,
 
         @field:JsonProperty("location")
-        val location: String? = null,
+        val location: Array<String>? = null,
 
         @field:JsonProperty("direction")
-        val direction: String? = null,
+        val direction: Array<String>? = null,
 
         @field:JsonProperty("distance")
-        val distance: String? = null,
+        val distance: Array<String>? = null,
 
         @field:JsonProperty("street")
-        val street: String? = null
-)
+        val street: Array<String>? = null
+) {
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (other !is StreetNumber) return false
+
+                if (number != null) {
+                        if (other.number == null) return false
+                        if (!number.contentEquals(other.number)) return false
+                } else if (other.number != null) return false
+                if (location != null) {
+                        if (other.location == null) return false
+                        if (!location.contentEquals(other.location)) return false
+                } else if (other.location != null) return false
+                if (direction != null) {
+                        if (other.direction == null) return false
+                        if (!direction.contentEquals(other.direction)) return false
+                } else if (other.direction != null) return false
+                if (distance != null) {
+                        if (other.distance == null) return false
+                        if (!distance.contentEquals(other.distance)) return false
+                } else if (other.distance != null) return false
+                if (street != null) {
+                        if (other.street == null) return false
+                        if (!street.contentEquals(other.street)) return false
+                } else if (other.street != null) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = number?.contentHashCode() ?: 0
+                result = 31 * result + (location?.contentHashCode() ?: 0)
+                result = 31 * result + (direction?.contentHashCode() ?: 0)
+                result = 31 * result + (distance?.contentHashCode() ?: 0)
+                result = 31 * result + (street?.contentHashCode() ?: 0)
+                return result
+        }
+}
