686d2e6f2ca58f4241c60568b7f091aaeba9c79f
Peter Wu <piterwu@outlook.com>
2024-06-03 15:23:38 +0800

    请求异常调整

--- tools/tools/src/main/kotlin/top/bettercode/summer/tools/lang/client/ApiTemplate.kt
+++ tools/tools/src/main/kotlin/top/bettercode/summer/tools/lang/client/ApiTemplate.kt
@@ -12,6 +12,7 @@ import org.springframework.lang.Nullable
 import org.springframework.web.client.*
 import java.io.IOException
 import java.lang.reflect.Type
+import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.URI
 import java.net.UnknownHostException
@@ -66,7 +67,7 @@ open class ApiTemplate<P : ClientProperties> @JvmOverloads constructor(
             if (e is ClientException || e is RestClientResponseException)
                 throw e
             else {
-                if (e.cause is SocketTimeoutException || e.cause is UnknownHostException) {
+                if (e.cause is SocketTimeoutException || e.cause is ConnectException || e.cause is UnknownHostException) {
                     throw clientException(message = "连接超时", cause = e)
                 } else
                     throw clientException(e)
