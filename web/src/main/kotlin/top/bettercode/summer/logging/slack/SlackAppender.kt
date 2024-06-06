ed465fd24e601491eed4da08fbfa8de1703808b4
Peter Wu <piterwu@outlook.com>
2024-06-06 15:58:52 +0800

    增加slack异常处理日志

--- web/src/main/kotlin/top/bettercode/summer/logging/slack/SlackAppender.kt
+++ web/src/main/kotlin/top/bettercode/summer/logging/slack/SlackAppender.kt
@@ -44,6 +44,29 @@ open class SlackAppender(
                 )
                 if (e.cause is SSLHandshakeException) {
                     channelExist = false
+                    log.warn(
+                        MarkerFactory.getMarker(NO_ALARM_LOG_MARKER),
+                        """Java 的信任库中没有包含所需的证书。解决这个问题的方法如下：
+导入目标服务器的证书到 Java 信任库
+
+首先，从目标服务器（https://slack.com）下载其 SSL 证书。你可以使用浏览器或者命令行工具来完成这个任务。然后使用 keytool 工具将证书导入到 Java 的信任库中。
+步骤：
+
+    获取证书：
+
+    bash
+
+echo | openssl s_client -connect slack.com:443 | openssl x509 > slack-com.crt
+
+导入证书到 Java 信任库：
+
+bash
+
+keytool -import -alias slack-com -keystore ${'$'}JAVA_HOME/lib/security/cacerts -file slack-com.crt
+
+默认密码是 changeit。
+"""
+                    )
                 }
             }
         }
