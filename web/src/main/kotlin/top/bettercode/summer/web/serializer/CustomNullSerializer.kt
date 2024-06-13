f601d574d5654cada2a938cc3dbfaea7e146e1e3
Peter Wu <piterwu@outlook.com>
2024-06-13 11:13:03 +0800

    json null 对象默认序列化支持

--- web/src/main/kotlin/top/bettercode/summer/web/serializer/CustomNullSerializer.kt
+++ web/src/main/kotlin/top/bettercode/summer/web/serializer/CustomNullSerializer.kt
@@ -9,6 +9,7 @@ import top.bettercode.summer.tools.lang.util.DirectFieldAccessFallbackBeanWrappe
 import top.bettercode.summer.web.properties.JacksonExtProperties
 import top.bettercode.summer.web.serializer.annotation.JsonBigDecimal
 import top.bettercode.summer.web.serializer.annotation.JsonCode
+import top.bettercode.summer.web.serializer.annotation.JsonDefault
 import top.bettercode.summer.web.serializer.annotation.JsonUrl
 import top.bettercode.summer.web.support.ApplicationContextHolder
 import java.io.Serializable
@@ -18,13 +19,16 @@ import java.util.*
 /**
  * @author Peter Wu
  */
-class CustomNullSerializer(private val writer: BeanPropertyWriter,
-                           private val defaultValue: String?,
-                           private val fieldName: String?,
-                           private val extendedValue: String?,
-                           private val jacksonExtProperties: JacksonExtProperties) : StdSerializer<Any>(Any::class.java) {
+class CustomNullSerializer(
+    private val writer: BeanPropertyWriter,
+    private val defaultValue: String?,
+    private val fieldName: String?,
+    private val extendedValue: String?,
+    private val jacksonExtProperties: JacksonExtProperties
+) : StdSerializer<Any>(Any::class.java) {
     private val type: Class<*> = writer.type.rawClass
-    private val isArray: Boolean = type.isArray || MutableCollection::class.java.isAssignableFrom(type) && !MutableMap::class.java
+    private val isArray: Boolean =
+        type.isArray || MutableCollection::class.java.isAssignableFrom(type) && !MutableMap::class.java
             .isAssignableFrom(type)
 
     override fun serialize(value: Any?, gen: JsonGenerator, provider: SerializerProvider) {
@@ -34,7 +38,8 @@ class CustomNullSerializer(private val writer: BeanPropertyWriter,
         if ("" == this.defaultValue && !this.fieldName.isNullOrBlank()) {
             val o = gen.currentValue
             val beanWrapper = DirectFieldAccessFallbackBeanWrapper(
-                    o)
+                o
+            )
             defaultValue = beanWrapper.getPropertyValue(this.fieldName)
         }
         if (defaultValue == null) {
@@ -55,37 +60,49 @@ class CustomNullSerializer(private val writer: BeanPropertyWriter,
             val `val` = ApplicationContextHolder.conversionService.convert(defaultValue, type)
             if (jsonBigDecimal != null) {
                 val divisor = jsonBigDecimal.divisor
-                BigDecimalSerializer(jsonBigDecimal.scale, if ("" == divisor) null else BigDecimal(divisor), jsonBigDecimal.roundingMode,
-                        jsonBigDecimal.toPlainString,
-                        jsonBigDecimal.reduceFraction, jsonBigDecimal.percent).serialize(
-                        `val` as BigDecimal, gen,
-                        provider)
+                BigDecimalSerializer(
+                    jsonBigDecimal.scale,
+                    if ("" == divisor) null else BigDecimal(divisor),
+                    jsonBigDecimal.roundingMode,
+                    jsonBigDecimal.toPlainString,
+                    jsonBigDecimal.reduceFraction,
+                    jsonBigDecimal.percent
+                ).serialize(
+                    `val` as BigDecimal, gen,
+                    provider
+                )
                 return
             }
             val jsonCode = writer.getAnnotation(JsonCode::class.java)
             if (jsonCode != null) {
                 CodeSerializer(jsonCode.value, jsonCode.extended)
-                        .serialize(`val` as Serializable, gen, provider)
+                    .serialize(`val` as Serializable, gen, provider)
                 return
             }
             val jsonUrl = writer.getAnnotation(JsonUrl::class.java)
             if (jsonUrl != null && `val` != null) {
-                UrlSerializer(jsonUrl.value, jsonUrl.urlFieldName, jsonUrl.extended,
-                        jsonUrl.asMap,
-                        jsonUrl.separator, jsonUrl.defaultValue, jsonUrl.mapper).serialize(`val`, gen,
-                        provider)
+                UrlSerializer(
+                    jsonUrl.value, jsonUrl.urlFieldName, jsonUrl.extended,
+                    jsonUrl.asMap,
+                    jsonUrl.separator, jsonUrl.defaultValue, jsonUrl.mapper
+                ).serialize(
+                    `val`, gen,
+                    provider
+                )
                 return
             }
             gen.writeObject(`val`)
         }
     }
 
-    private fun serializeJsonCode(value: Any?, gen: JsonGenerator, provider: SerializerProvider,
-                                  fieldName: String): Boolean {
+    private fun serializeJsonCode(
+        value: Any?, gen: JsonGenerator, provider: SerializerProvider,
+        fieldName: String
+    ): Boolean {
         val jsonCode = writer.getAnnotation(JsonCode::class.java)
         if (jsonCode != null) {
             CodeSerializer(jsonCode.value, jsonCode.extended)
-                    .serialize("", gen, provider)
+                .serialize("", gen, provider)
             return true
         }
         serializeNull(gen, type, value)
@@ -93,9 +110,12 @@ class CustomNullSerializer(private val writer: BeanPropertyWriter,
         return false
     }
 
-    private fun serializeExtend(gen: JsonGenerator, fieldName: String,
-                                defaultEmpty: Boolean) {
-        val value = if (!extendedValue.isNullOrBlank()) extendedValue else if (defaultEmpty) "" else null
+    private fun serializeExtend(
+        gen: JsonGenerator, fieldName: String,
+        defaultEmpty: Boolean
+    ) {
+        val value =
+            if (!extendedValue.isNullOrBlank()) extendedValue else if (defaultEmpty) "" else null
         val jsonCode = writer.getAnnotation(JsonCode::class.java)
         if (jsonCode != null && jsonCode.extended) {
             gen.writeStringField(fieldName + "Name", value)
@@ -117,7 +137,10 @@ class CustomNullSerializer(private val writer: BeanPropertyWriter,
                 }
             }
             if (isArray) {
-                gen.writeObjectField(urlFieldName, if (defaultEmpty) Collections.EMPTY_LIST else null)
+                gen.writeObjectField(
+                    urlFieldName,
+                    if (defaultEmpty) Collections.EMPTY_LIST else null
+                )
             } else {
                 gen.writeStringField(urlFieldName, value)
             }
@@ -129,24 +152,31 @@ class CustomNullSerializer(private val writer: BeanPropertyWriter,
             gen.writeString("")
         } else if (isArray) {
             gen.writeObject(Collections.EMPTY_LIST)
-        } else if (jacksonExtProperties.isNullObjectAsEmpty && (type.classLoader != null || MutableMap::class.java.isAssignableFrom(type))) {
+        } else if ((jacksonExtProperties.isNullObjectAsEmpty || writer.getAnnotation(JsonDefault::class.java) != null) && (type.classLoader != null || MutableMap::class.java.isAssignableFrom(
+                type
+            ))
+        ) {
             gen.writeObject(emptyMap<Any, Any>())
         } else {
             gen.writeObject(value)
         }
     }
 
-    override fun serializeWithType(value: Any, gen: JsonGenerator, provider: SerializerProvider,
-                                   typeSer: TypeSerializer) {
+    override fun serializeWithType(
+        value: Any, gen: JsonGenerator, provider: SerializerProvider,
+        typeSer: TypeSerializer
+    ) {
         serialize(value, gen, provider)
     }
 
     companion object {
         fun support(type: Class<*>): Boolean {
-            return type == String::class.java || type.isArray || (MutableCollection::class.java.isAssignableFrom(type)
+            return type == String::class.java || type.isArray || (MutableCollection::class.java.isAssignableFrom(
+                type
+            )
                     && !MutableMap::class.java
-                    .isAssignableFrom(type)) || type.classLoader != null || MutableMap::class.java
-                    .isAssignableFrom(type)
+                .isAssignableFrom(type)) || type.classLoader != null || MutableMap::class.java
+                .isAssignableFrom(type)
         }
     }
 }
