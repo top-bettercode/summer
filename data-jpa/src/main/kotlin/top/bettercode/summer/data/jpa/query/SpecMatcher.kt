2884b121b3e198121620595e12c404a94e3652f7
Peter Wu <piterwu@outlook.com>
2024-06-13 10:33:33 +0800

    Match更新异常修复

--- data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/query/SpecMatcher.kt
+++ data-jpa/src/main/kotlin/top/bettercode/summer/data/jpa/query/SpecMatcher.kt
@@ -22,7 +22,7 @@ import javax.persistence.metamodel.ManagedType
  * @author Peter Wu
  */
 open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
-        val matchMode: SpecMatcherMode, private val probe: Any?
+    val matchMode: SpecMatcherMode, private val probe: Any?
 ) : UpdateSpecification<T>, SpecPredicate<T, M> {
 
     private val log: Logger = LoggerFactory.getLogger(SpecMatcher::class.java)
@@ -40,7 +40,11 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
         typed = this as M
     }
 
-    override fun toPredicate(root: Root<T>, query: CriteriaQuery<*>, cb: CriteriaBuilder): Predicate? {
+    override fun toPredicate(
+        root: Root<T>,
+        query: CriteriaQuery<*>,
+        cb: CriteriaBuilder
+    ): Predicate? {
         if (orders.isNotEmpty()) {
             val orders = orders.map { o: Sort.Order ->
                 val path: Path<*> = SpecPath.toPath(root, o.property)
@@ -53,8 +57,10 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
 
     override fun toPredicate(root: Root<T>, criteriaBuilder: CriteriaBuilder): Predicate? {
         if (probe != null) {
-            setPathDefaultValue("", root, root.model, probe, probe.javaClass,
-                    PathNode("root", null, probe))
+            setPathDefaultValue(
+                "", root, root.model, probe, probe.javaClass,
+                PathNode("root", null, probe)
+            )
         }
         val predicates: MutableList<Predicate> = ArrayList()
         for (values in specPredicates.values) {
@@ -75,10 +81,19 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
             return null
         }
         val restrictions = predicates.toTypedArray<Predicate>()
-        return if (matchMode == SpecMatcherMode.ALL) criteriaBuilder.and(*restrictions) else criteriaBuilder.or(*restrictions)
+        return if (matchMode == SpecMatcherMode.ALL) criteriaBuilder.and(*restrictions) else criteriaBuilder.or(
+            *restrictions
+        )
     }
 
-    private fun setPathDefaultValue(path: String, from: Path<*>, type: ManagedType<*>, probe: Any, probeType: Class<*>, currentNode: PathNode) {
+    private fun setPathDefaultValue(
+        path: String,
+        from: Path<*>,
+        type: ManagedType<*>,
+        probe: Any,
+        probeType: Class<*>,
+        currentNode: PathNode
+    ) {
         val beanWrapper = DirectFieldAccessFallbackBeanWrapper(probe)
         for (attribute in type.singularAttributes) {
             val currentPath = if (path.isBlank()) attribute.name else path + "." + attribute.name
@@ -95,21 +110,31 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
             if (attributeValue == null || "" == attributeValue) {
                 continue
             }
-            if (attribute.persistentAttributeType == PersistentAttributeType.EMBEDDED || isAssociation(attribute) && from !is From<*, *>) {
-                setPathDefaultValue(currentPath, from.get<Any>(attribute.name),
-                        attribute.type as ManagedType<*>, attributeValue, probeType, currentNode)
+            if (attribute.persistentAttributeType == PersistentAttributeType.EMBEDDED || isAssociation(
+                    attribute
+                ) && from !is From<*, *>
+            ) {
+                setPathDefaultValue(
+                    currentPath, from.get<Any>(attribute.name),
+                    attribute.type as ManagedType<*>, attributeValue, probeType, currentNode
+                )
                 continue
             }
             if (isAssociation(attribute)) {
                 val node = currentNode.add(attribute.name, attributeValue)
                 if (node.spansCycle()) {
-                    throw InvalidDataAccessApiUsageException(String.format(
+                    throw InvalidDataAccessApiUsageException(
+                        String.format(
                             "Path '%s' from root %s must not span a cyclic property reference!\r\n%s",
                             currentPath,
-                            ClassUtils.getShortName(probeType), node))
+                            ClassUtils.getShortName(probeType), node
+                        )
+                    )
                 }
-                setPathDefaultValue(currentPath, (from as From<*, *>).join<Any, Any>(attribute.name),
-                        attribute.type as ManagedType<*>, attributeValue, probeType, node)
+                setPathDefaultValue(
+                    currentPath, (from as From<*, *>).join<Any, Any>(attribute.name),
+                    attribute.type as ManagedType<*>, attributeValue, probeType, node
+                )
                 continue
             }
             if (!specPath.isSetCriteria) {
@@ -118,23 +143,30 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
         }
     }
 
-    override fun createCriteriaUpdate(domainClass: Class<T>, criteriaBuilder: CriteriaBuilder, extJpaSupport: ExtJpaSupport<T>): CriteriaUpdate<T> {
+    override fun createCriteriaUpdate(
+        domainClass: Class<T>,
+        criteriaBuilder: CriteriaBuilder,
+        extJpaSupport: ExtJpaSupport<T>
+    ): CriteriaUpdate<T> {
         val criteriaUpdate = criteriaBuilder.createCriteriaUpdate(domainClass)
         criteriaUpdate.from(domainClass)
-        specPredicates.values.map { it.first() }.filter { it is SpecPath<*, *, *> }.map { it as SpecPath<*, T, M> }.forEach {
-            if (it.isSetCriteriaUpdate) {
-                criteriaUpdate[it.propertyName] = it.criteriaUpdate
-            }
-            val idAttribute = extJpaSupport.idAttribute
-            if (idAttribute != null && it.propertyName == idAttribute.name) {
-                this.idAttribute = SingularAttributeValue(idAttribute, it.criteria!!)
-            } else {
-                val versionAttribute = extJpaSupport.versionAttribute
-                if (versionAttribute != null && it.propertyName == versionAttribute.name) {
-                    this.versionAttribute = SingularAttributeValue(versionAttribute, it.criteria!!)
+        specPredicates.values.map { it.first() }.filter { it is SpecPath<*, *, *> }
+            .map { it as SpecPath<*, T, M> }.forEach {
+                if (it.isSetCriteriaUpdate) {
+                    criteriaUpdate[it.propertyName] = it.criteriaUpdate
                 }
+                val idAttribute = extJpaSupport.idAttribute
+                val value = it.criteria ?: it.criterias
+                if (value != null)
+                    if (idAttribute != null && it.propertyName == idAttribute.name) {
+                        this.idAttribute = SingularAttributeValue(idAttribute, value)
+                    } else {
+                        val versionAttribute = extJpaSupport.versionAttribute
+                        if (versionAttribute != null && it.propertyName == versionAttribute.name) {
+                            this.versionAttribute = SingularAttributeValue(versionAttribute, value)
+                        }
+                    }
             }
-        }
 
         return criteriaUpdate
     }
@@ -152,8 +184,16 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
             append = false
             p
         } else
-            specPredicates.computeIfAbsent(propertyName
-            ) { s: String -> return@computeIfAbsent mutableListOf<SpecPredicate<T, M>>(SpecPath<P, T, M>(typed, s)) }.first()
+            specPredicates.computeIfAbsent(
+                propertyName
+            ) { s: String ->
+                return@computeIfAbsent mutableListOf<SpecPredicate<T, M>>(
+                    SpecPath<P, T, M>(
+                        typed,
+                        s
+                    )
+                )
+            }.first()
         @Suppress("UNCHECKED_CAST")
         return predicate as SpecPath<P, T, M>
     }
@@ -166,7 +206,8 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
     fun all(other: T?, matcher: (M) -> M): M {
         val constructor = typed.javaClass.declaredConstructors[0]
         constructor.isAccessible = true
-        @Suppress("UNCHECKED_CAST") val otherMatcher = constructor.newInstance(SpecMatcherMode.ALL, other) as M
+        @Suppress("UNCHECKED_CAST") val otherMatcher =
+            constructor.newInstance(SpecMatcherMode.ALL, other) as M
         matcher(otherMatcher)
         specPredicates["all" + UUID.randomUUID().toString()] = otherMatcher
         return typed
@@ -179,7 +220,8 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
     fun any(other: T?, matcher: (M) -> M): M {
         val constructor = typed.javaClass.declaredConstructors[0]
         constructor.isAccessible = true
-        @Suppress("UNCHECKED_CAST") val otherMatcher = constructor.newInstance(SpecMatcherMode.ANY, other) as M
+        @Suppress("UNCHECKED_CAST") val otherMatcher =
+            constructor.newInstance(SpecMatcherMode.ANY, other) as M
         matcher(otherMatcher)
         specPredicates["any" + UUID.randomUUID().toString()] = otherMatcher
         return typed
@@ -188,7 +230,7 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
     //--------------------------------------------
     fun sortBy(direction: Sort.Direction, vararg propertyName: String): M {
         orders.addAll(propertyName //
-                .map { it: String -> Sort.Order(direction, it) } //
+            .map { it: String -> Sort.Order(direction, it) } //
         )
         return typed
     }
@@ -299,10 +341,12 @@ open class SpecMatcher<T : Any?, M : SpecMatcher<T, M>> protected constructor(
     }
 
     companion object {
-        private val ASSOCIATION_TYPES: Set<PersistentAttributeType> = EnumSet.of(PersistentAttributeType.MANY_TO_MANY,  //
-                PersistentAttributeType.MANY_TO_ONE,  //
-                PersistentAttributeType.ONE_TO_MANY,  //
-                PersistentAttributeType.ONE_TO_ONE)
+        private val ASSOCIATION_TYPES: Set<PersistentAttributeType> = EnumSet.of(
+            PersistentAttributeType.MANY_TO_MANY,  //
+            PersistentAttributeType.MANY_TO_ONE,  //
+            PersistentAttributeType.ONE_TO_MANY,  //
+            PersistentAttributeType.ONE_TO_ONE
+        )
 
         private fun isAssociation(attribute: Attribute<*, *>): Boolean {
             return ASSOCIATION_TYPES.contains(attribute.persistentAttributeType)
