d5d27c471c848fdce547c5277f60fb61a80635bd
Peter Wu <piterwu@outlook.com>
2024-10-11 11:47:00 +0800

    增加海康门禁可用配置

--- clients/hikvision/src/main/kotlin/top/bettercode/summer/tools/hikvision/HikvisionConfiguration.kt
+++ clients/hikvision/src/main/kotlin/top/bettercode/summer/tools/hikvision/HikvisionConfiguration.kt
@@ -1,5 +1,6 @@
 package top.bettercode.summer.tools.hikvision
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration
  *
  * @author Peter Wu
  */
+@ConditionalOnProperty(prefix = "summer.hikvision", name = ["host"])
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(HikvisionProperties::class)
 class HikvisionConfiguration {
