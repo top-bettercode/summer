1178a63de7b3265915937c121410dc5fb61f206c
Peter Wu <piterwu@outlook.com>
2024-10-22 11:29:55 +0800

    jpush异常响应调整

--- clients/jpush/src/main/kotlin/top/bettercode/summer/tools/jpush/entity/resp/JpushErrorResponse.kt
+++ clients/jpush/src/main/kotlin/top/bettercode/summer/tools/jpush/entity/resp/JpushErrorResponse.kt
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 data class JpushErrorResponse(
 
+        @field:JsonProperty("msg_id")
+        val msgId: String? = null,
+
         @field:JsonProperty("error")
         val error: Error? = null
 )
