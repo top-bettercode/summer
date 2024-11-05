b7f25a1a8e38431002528f6eda08e26ceeefa459
Peter Wu <piterwu@outlook.com>
2024-11-05 13:44:53 +0800

    amap增加缓存

--- clients/amap/src/main/kotlin/top/bettercode/summer/tools/amap/AMapClient.kt
+++ clients/amap/src/main/kotlin/top/bettercode/summer/tools/amap/AMapClient.kt
@@ -1,6 +1,7 @@
 package top.bettercode.summer.tools.amap
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -15,6 +16,7 @@ import top.bettercode.summer.tools.amap.entity.AMapRegeoResp
 import top.bettercode.summer.tools.amap.entity.DistanceResp
 import top.bettercode.summer.tools.amap.entity.Extensions
 import top.bettercode.summer.tools.lang.client.ApiTemplate
+import java.util.concurrent.TimeUnit
 
 /**
  *
@@ -28,6 +30,16 @@ open class AMapClient(properties: AMapProperties) : ApiTemplate<AMapProperties>(
     companion object {
         const val MARKER = "amap"
     }
+
+    private val regeoCache = Caffeine.newBuilder()
+        .expireAfterWrite(1, TimeUnit.DAYS)
+        .maximumSize(100).build<String, AMapRegeoResp>().asMap()
+    private val geoCache = Caffeine.newBuilder()
+        .expireAfterWrite(1, TimeUnit.DAYS)
+        .maximumSize(100).build<String, AMapGeoResp>().asMap()
+    private val distanceCache = Caffeine.newBuilder()
+        .expireAfterWrite(1, TimeUnit.DAYS)
+        .maximumSize(100).build<String, DistanceResp>().asMap()
 
     init {
         val messageConverter: MappingJackson2HttpMessageConverter =
@@ -57,50 +69,59 @@ open class AMapClient(properties: AMapProperties) : ApiTemplate<AMapProperties>(
      */
     @JvmOverloads
     open fun regeo(location: String, extensions: Extensions = Extensions.BASE): AMapRegeoResp {
-        //restapi.amap.com/v3/geocode/regeo?key=您的key&location=116.481488,39.990464&poitype=&radius=&extensions=base&batch=false&roadlevel=0
-        val entity: ResponseEntity<AMapRegeoResp> =
-            exchange(
-                properties.url + "/geocode/regeo?key={0}&location={1}&extensions={2}",
-                HttpMethod.GET,
-                null,
-                AMapRegeoResp::class.java,
-                properties.key,
-                location, extensions.name.lowercase()
-            ) ?: throw clientException()
-        return entity.body ?: throw clientException()
+        val key = "regeo::$location::$extensions"
+        return regeoCache.computeIfAbsent(key) {
+            //restapi.amap.com/v3/geocode/regeo?key=您的key&location=116.481488,39.990464&poitype=&radius=&extensions=base&batch=false&roadlevel=0
+            val entity: ResponseEntity<AMapRegeoResp> =
+                exchange(
+                    properties.url + "/geocode/regeo?key={0}&location={1}&extensions={2}",
+                    HttpMethod.GET,
+                    null,
+                    AMapRegeoResp::class.java,
+                    properties.key,
+                    location, extensions.name.lowercase()
+                ) ?: throw clientException()
+            return@computeIfAbsent entity.body ?: throw clientException()
+        }
     }
 
     /**
      * 地理编码 https://lbs.amap.com/api/webservice/guide/api/georegeo
      */
     open fun geo(address: String): AMapGeoResp {
-        //https://restapi.amap.com/v3/geocode/geo?address=北京市朝阳区阜通东大街6号&output=JSON&key=您的key
-        val entity: ResponseEntity<AMapGeoResp> =
-            exchange(
-                properties.url + "/geocode/geo?key={0}&address={1}&output=JSON",
-                HttpMethod.GET,
-                null,
-                AMapGeoResp::class.java,
-                properties.key,
-                address
-            ) ?: throw clientException("请求失败")
+        val key = "geo::$address"
+        return geoCache.computeIfAbsent(key) {
+            //https://restapi.amap.com/v3/geocode/geo?address=北京市朝阳区阜通东大街6号&output=JSON&key=您的key
+            val entity: ResponseEntity<AMapGeoResp> =
+                exchange(
+                    properties.url + "/geocode/geo?key={0}&address={1}&output=JSON",
+                    HttpMethod.GET,
+                    null,
+                    AMapGeoResp::class.java,
+                    properties.key,
+                    address
+                ) ?: throw clientException("请求失败")
 
-        return entity.body ?: throw clientException()
+            return@computeIfAbsent entity.body ?: throw clientException()
+        }
     }
 
     open fun distance(origins: String, destination: String): DistanceResp {
-        val entity: ResponseEntity<DistanceResp> =
-            exchange(
-                properties.url + "/distance?key={0}&origins={1}&destination={2}&output=JSON",
-                HttpMethod.GET,
-                null,
-                DistanceResp::class.java,
-                properties.key,
-                origins,
-                destination
-            ) ?: throw clientException("请求失败")
+        val key = "distance::$origins::$destination"
+        return distanceCache.computeIfAbsent(key) {
+            val entity: ResponseEntity<DistanceResp> =
+                exchange(
+                    properties.url + "/distance?key={0}&origins={1}&destination={2}&output=JSON",
+                    HttpMethod.GET,
+                    null,
+                    DistanceResp::class.java,
+                    properties.key,
+                    origins,
+                    destination
+                ) ?: throw clientException("请求失败")
 
-        return entity.body ?: throw clientException()
+            return@computeIfAbsent entity.body ?: throw clientException()
+        }
     }
 
 }
