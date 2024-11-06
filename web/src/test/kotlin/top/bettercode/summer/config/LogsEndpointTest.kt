8d227161d71937da148bf7fc079f011b1eef7ac3
Peter Wu <piterwu@outlook.com>
2024-11-07 01:51:30 +0800

    测试

--- web/src/test/kotlin/top/bettercode/summer/config/LogsEndpointTest.kt
+++ web/src/test/kotlin/top/bettercode/summer/config/LogsEndpointTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
 import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties
 import org.springframework.mock.env.MockEnvironment
+import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpServletResponse
 import top.bettercode.summer.logging.WebsocketProperties
 import java.io.ByteArrayInputStream
@@ -19,11 +20,13 @@ class LogsEndpointTest {
     @Disabled
     @Test
     fun path() {
+        val request = MockHttpServletRequest()
         val response = MockHttpServletResponse()
         val logsEndpoint = LogsEndpoint(
             "/local/downloads",
             MockEnvironment(),
             WebsocketProperties(),
+            request,
             response,
             WebEndpointProperties(),
             ManagementServerProperties()
