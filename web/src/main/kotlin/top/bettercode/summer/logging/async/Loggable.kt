26eb4fece0a11b25af1888d1fc18b8e1fd8be304
Peter Wu <piterwu@outlook.com>
2024-11-08 14:20:56 +0800

    日志调整

--- web/src/main/kotlin/top/bettercode/summer/logging/async/Loggable.kt
+++ web/src/main/kotlin/top/bettercode/summer/logging/async/Loggable.kt
@@ -1,6 +1,8 @@
 package top.bettercode.summer.logging.async
 
+import org.slf4j.event.Level
+
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
-annotation class Loggable
+annotation class Loggable(val value: Level)
--- web/src/main/kotlin/top/bettercode/summer/logging/async/MethodLoggingAspect.kt
+++ web/src/main/kotlin/top/bettercode/summer/logging/async/MethodLoggingAspect.kt
@@ -3,11 +3,14 @@ package top.bettercode.summer.logging.async
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.reflect.MethodSignature
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
+import org.slf4j.event.Level
 import top.bettercode.summer.tools.lang.log.AlarmMarker
 import top.bettercode.summer.tools.lang.operation.HttpOperation
+
 
 @Aspect
 class MethodLoggingAspect {
@@ -21,18 +24,23 @@ class MethodLoggingAspect {
     @Around("@annotation(org.springframework.scheduling.annotation.Async) || @annotation(org.springframework.scheduling.annotation.Scheduled) || @annotation(top.bettercode.summer.logging.async.Loggable)")
     @Throws(Throwable::class)
     fun logProceed(joinPoint: ProceedingJoinPoint): Any? {
-        val methodName = joinPoint.signature.toShortString()
+        val signature = joinPoint.signature as MethodSignature
+        val methodName = signature.toShortString()
         try {
+            val method = signature.method
+            val annotation = method.getAnnotation(Loggable::class.java)
+            val level = annotation?.value ?: Level.INFO
+
             val startTime = System.currentTimeMillis()
             val traceid = HttpOperation.traceid()
             MDC.put(HttpOperation.MDC_TRACEID, traceid)
-            logger.info("==={} started===", methodName)
+            log(level, "==={} started===", methodName)
             val result = joinPoint.proceed()
             val cost = System.currentTimeMillis() - startTime
             if (cost > QUERIES_SLOWER_THAN_MS) {
                 logger.warn("==={} finished cost: {} ms===", methodName, cost)
             } else {
-                logger.info("==={} finished cost: {} ms===", methodName, cost)
+                log(level, "==={} finished cost: {} ms===", methodName, cost)
             }
             return result
         } catch (e: Throwable) {
@@ -40,6 +48,17 @@ class MethodLoggingAspect {
             throw e
         } finally {
             MDC.remove(HttpOperation.MDC_TRACEID)
+        }
+    }
+
+    private fun log(level: Level, message: String, vararg arguments: Any) {
+        when (level) {
+            Level.TRACE -> logger.trace(message, *arguments)
+            Level.DEBUG -> logger.debug(message, *arguments)
+            Level.INFO -> logger.info(message, *arguments)
+            Level.WARN -> logger.warn(message, *arguments)
+            Level.ERROR -> logger.error(message, *arguments)
+            else -> logger.info(message, *arguments)
         }
     }
 }
