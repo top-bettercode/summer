2740eb94f957b6062020bb2b081659285d617bb1
Peter Wu <piterwu@outlook.com>
2024-11-27 09:23:58 +0800

    批量查询库存调整

--- natives/sap/src/main/kotlin/top/bettercode/summer/tools/sap/stock/SapStockService.kt
+++ natives/sap/src/main/kotlin/top/bettercode/summer/tools/sap/stock/SapStockService.kt
@@ -1,27 +1,16 @@
 package top.bettercode.summer.tools.sap.stock
 
+import kotlinx.coroutines.*
 import org.slf4j.LoggerFactory
 import top.bettercode.summer.tools.lang.client.ClientSysException
 import top.bettercode.summer.tools.sap.connection.SapService
 import top.bettercode.summer.tools.sap.connection.pojo.SapHead
 import java.math.BigDecimal
-import java.util.concurrent.*
 
-class SapStockService {
+class SapStockService(private val sapService: SapService) {
+
     private val log = LoggerFactory.getLogger(SapStockService::class.java)
-    private val sapService: SapService
-    private val executor: ThreadPoolExecutor
-
-    constructor(sapService: SapService) {
-        this.sapService = sapService
-        executor = ThreadPoolExecutor(10, 100, 0L, TimeUnit.MILLISECONDS,
-                LinkedBlockingQueue(16), ThreadPoolExecutor.CallerRunsPolicy())
-    }
-
-    constructor(sapService: SapService, executor: ThreadPoolExecutor) {
-        this.sapService = sapService
-        this.executor = executor
-    }
+    private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
 
     fun getStore(materialId: String?, factoryId: String?): StockLsReturn? {
         return try {
@@ -32,7 +21,8 @@ class SapStockService {
             head.ifid = "STOCK_QUERY_IN"
             head.sysid = "CRM2"
             sapQueryStock.head = head
-            val stockResp = sapService.invoke("ZRFC_STOCK_001", sapQueryStock, StockResp::class.java)
+            val stockResp =
+                sapService.invoke("ZRFC_STOCK_001", sapQueryStock, StockResp::class.java)
             stockResp.lsReturn
         } catch (e: ClientSysException) {
             val lsReturn = StockLsReturn()
@@ -48,32 +38,13 @@ class SapStockService {
         }
     }
 
-    fun getStores(params: Set<StockQuery>): Map<StockQuery, StockLsReturn> {
-        return try {
-            val countDownLatch = CountDownLatch(params.size)
-            val futures: MutableMap<StockQuery, Future<StockLsReturn>> = ConcurrentHashMap()
-            for (param in params) {
-                val future = executor.submit<StockLsReturn> {
-                    try {
-                        return@submit getStore(param.materialId, param.factoryId)
-                    } finally {
-                        countDownLatch.countDown()
-                    }
+    fun getStores(params: Set<StockQuery>): Map<StockQuery, StockLsReturn?> {
+        return runBlocking {
+            params.map {
+                scope.async {
+                    it to getStore(it.materialId, it.factoryId)
                 }
-                futures[param] = future
-            }
-            countDownLatch.await()
-            val results: MutableMap<StockQuery, StockLsReturn> = ConcurrentHashMap()
-            for ((key, value) in futures) {
-                try {
-                    results[key] = value.get()
-                } catch (e: ExecutionException) {
-                    throw RuntimeException(e)
-                }
-            }
-            results
-        } catch (e: InterruptedException) {
-            throw RuntimeException(e)
+            }.awaitAll().toMap()
         }
     }
 }
